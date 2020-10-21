d147ddf68be982124d6fff6649b1466dfc08fc41
TurBoss <j.l.toledano.l@gmail.com>
2020-10-21 05:53:35 +0200

    try to fix Auth

--- user_auth.py
+++ user_auth.py
@@ -5,7 +5,7 @@ import passwords
 
 class UserAuth(util.CustomAuth):
     def check_credentials(self, user, password):
-        if user == b'admin' and str(password) == str(passwords.web_password):
+        if user == b'admin' and password == passwords.web_password.encode('UTF-8'):
             return True
         else:
             return False
