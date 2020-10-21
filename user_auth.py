a0f85bde35a1a393c1ba73d7c61e633a35ea002d
TurBoss <j.l.toledano.l@gmail.com>
2020-10-21 05:45:45 +0200

    more string checks

--- user_auth.py
+++ user_auth.py
@@ -5,7 +5,7 @@ import passwords
 
 class UserAuth(util.CustomAuth):
     def check_credentials(self, user, password):
-        if user == b'admin' and password == str(passwords.web_password):
+        if user == b'admin' and str(password) == str(passwords.web_password):
             return True
         else:
             return False
