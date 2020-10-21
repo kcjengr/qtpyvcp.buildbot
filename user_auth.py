7b5dc1d1573fb9c739455aaf4a2509d4fd7860f6
TurBoss <j.l.toledano.l@gmail.com>
2020-10-21 06:04:39 +0200

    fix3

--- user_auth.py
+++ user_auth.py
@@ -1,11 +1,10 @@
 from buildbot.plugins import util
-
-from pass_file import web_password
+import pass_file
 
 
 class UserAuth(util.CustomAuth):
     def check_credentials(self, user, password):
-        if user == b'admin' and password == web_password:
+        if user == b'admin' and password == pass_file.web_password:
             return True
         else:
             return False
