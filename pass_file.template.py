511272111e5c7fe6043d6ed9be83fd92da3fae38
TurBoss <j.l.toledano.l@gmail.com>
2021-02-20 18:45:47 +0100

    fix auth template variables

--- pass_file.template.py
+++ pass_file.template.py
@@ -1,9 +1,8 @@
-matrix_access_token = ''
 github_phrase = ''
 web_password = ''
+worker = ''
 
-github_qtpyvcp_auth = ''
-github_probe_basic_auth = ''
-
-pypi_qtpyvcp_auth = ''
-pypi_probe_basic_auth = ''
+matrix_access_token = ''
+github_kcjengr_token = ''
+pypi_qtpyvcp_token = ''
+pypi_probe_basic_token = ''
