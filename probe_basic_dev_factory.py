c33f924e19324c31446f1c71941016eccd60983d
TurBoss <j.l.toledano.l@gmail.com>
2021-02-28 01:05:51 +0100

    remove space

--- probe_basic_dev_factory.py
+++ probe_basic_dev_factory.py
@@ -141,5 +141,5 @@ factory_probe_basic_dev.addStep(
         workdir="docs/"))
  
 factory_probe_basic_dev.addStep(steps.ShellCommand(command=["git", "add", "."], workdir="docs/"))
-factory_probe_basic_dev.addStep(steps.ShellCommand(command=["git", "commit", "-a ", "-m", "deploy gh-pages"], workdir="docs/"))
+factory_probe_basic_dev.addStep(steps.ShellCommand(command=["git", "commit", "-a", "-m", "deploy gh-pages"], workdir="docs/"))
 # factory_probe_basic_dev.addStep(steps.ShellCommand(command=["git", "push", "."], workdir="docs/"))
