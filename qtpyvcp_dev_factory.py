9adfbea6c28d27de69fc4aba195ccf54a427203c
TurBoss <j.l.toledano.l@gmail.com>
2021-04-13 17:15:05 +0200

    fix path

--- qtpyvcp_dev_factory.py
+++ qtpyvcp_dev_factory.py
@@ -140,7 +140,7 @@ factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete dist directory", 
 factory_qtpyvcp_dev.addStep(
     steps.Sphinx(
         name="compile sphinx docs",
-        sphinx_builddir="/home/kcjengr/buildbot/worker/qtpyvcp-dev/sources/docs/source/",
+        # sphinx_builddir="/home/kcjengr/buildbot/worker/qtpyvcp-dev/docs",
         sphinx_sourcedir="/home/kcjengr/buildbot/worker/qtpyvcp-dev/sources/docs/source/",
         env={"VIRTUAL_ENV": "/home/kcjengr/buildbot/venvs/qtpyvcp_dev_venv/",
              "PATH": ["/home/kcjengr/buildbot/worker/qtpyvcp-dev/sources/qtpyvcp", "${PATH}"]},
