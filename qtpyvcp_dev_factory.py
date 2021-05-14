a65ef4adbfd1e72e5fae3cb16f5553184c01331d
TurBoss <j.l.toledano.l@gmail.com>
2021-05-15 01:45:36 +0200

    use system sphinx

--- qtpyvcp_dev_factory.py
+++ qtpyvcp_dev_factory.py
@@ -149,6 +149,7 @@ factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete docs directory", 
 factory_qtpyvcp_dev.addStep(
     steps.Sphinx(
         name="compile sphinx docs",
+        sphinx="/usr/bin/sphinx-build",
         sphinx_builddir="/home/buildbot/buildbot/worker/qtpyvcp-dev/docs",
         sphinx_sourcedir="/home/buildbot/buildbot/worker/qtpyvcp-dev/sources/docs/source/",
         strict_warnings=False,
