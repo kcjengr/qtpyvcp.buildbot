4cf7535fb211815624e0866ccea6e36b5f318009
TurBoss <j.l.toledano.l@gmail.com>
2021-05-17 06:44:57 +0200

    handle release

--- github_handler.py
+++ github_handler.py
@@ -1,10 +1,11 @@
 # -*- python -*-
 # ex: set syntax=python:
-
+import json
 import re
 from pprint import pprint
 
 from buildbot.www.hooks.github import GitHubEventHandler
+from dateutil.parser import parse as dateparse
 from twisted.python import log
 
 
@@ -21,14 +22,26 @@ class CustomGitHubEventHandler(GitHubEventHandler):
             log.msg("Got Push to master")
 
             return super().handle_push(payload, event)
-        elif re.match(r"^refs/(tags)/(v?[0-9]+\.?[0-99]+\.?[0-99]?.)$", ref):
 
-            pprint(payload.get("head_commit"))
-            payload["commits"] = payload.get("head_commit")
-            pprint(payload.get("commits"))
-            log.msg("Got Push to tags")
-
-            return super().handle_push(payload, event)
         else:
             print(f'CustomGitHubEventHandler: ignoring push event for ref: {ref}')
             return [], 'git'
+
+    def handle_release(self, payload, event):
+        if 'release' not in payload:
+            payload = json.loads(payload['payload'][0])
+        
+        change = {
+            'author': payload['release']['author']['login'],
+            'repository': payload['repository']['clone_url'],
+            'project': payload['repository']['full_name'],
+            'revision': payload['release']['tag_name'],
+            'when_timestamp': dateparse(payload['release']['published_at']),
+            'revlink': payload['release']['html_url'],
+            'category': 'release',
+            'comments': payload['release']['body'],
+            'branch': payload['release']['tag_name'],
+        }
+
+        # Do some magic here
+        return [change], 'git'
