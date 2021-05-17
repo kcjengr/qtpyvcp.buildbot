29e1843547e0fd968e6a8a28b87dd02f344ade7f
TurBoss <j.l.toledano.l@gmail.com>
2021-05-17 07:22:25 +0200

    trigger build on tags

--- github_handler.py
+++ github_handler.py
@@ -22,25 +22,24 @@ class CustomGitHubEventHandler(GitHubEventHandler):
             log.msg("Got Push to master")
 
             return super().handle_push(payload, event)
+        elif re.match(r"^refs/(tags)/(v?[0-9]+\.?[0-99]+\.?[0-99]?.)$", ref):
+            def handle_release(self, payload, event):
+                if 'release' not in payload:
+                    payload = json.loads(payload['payload'][0])
 
+                change = {
+                    'author': payload['release']['author']['login'],
+                    'repository': payload['repository']['clone_url'],
+                    'project': payload['repository']['full_name'],
+                    'revision': payload['release']['tag_name'],
+                    'when_timestamp': dateparse(payload['release']['published_at']),
+                    'revlink': payload['release']['html_url'],
+                    'category': 'release',
+                    'comments': payload['release']['body'],
+                    'branch': payload['release']['tag_name'],
+                }
+                # Do some magic here
+                return [change], 'git'
         else:
             print(f'CustomGitHubEventHandler: ignoring push event for ref: {ref}')
             return [], 'git'
-
-    def handle_release(self, payload, event):
-        if 'release' not in payload:
-            payload = json.loads(payload['payload'][0])
-
-        change = {
-            'author': payload['release']['author']['login'],
-            'repository': payload['repository']['clone_url'],
-            'project': payload['repository']['full_name'],
-            'revision': payload['release']['tag_name'],
-            'when_timestamp': dateparse(payload['release']['published_at']),
-            'revlink': payload['release']['html_url'],
-            'category': 'release',
-            'comments': payload['release']['body'],
-            'branch': payload['release']['tag_name'],
-        }
-        # Do some magic here
-        return [change], 'git'
