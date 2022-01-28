c0ad8717b8e48230c92d2e3f19e3a40a141ec6c1
TurBoss <j.l.toledano.l@gmail.com>
2022-01-28 06:43:14 +0100

    rename factory

--- probe_basic_py2_factory.py
+++ probe_basic_py2_factory.py
@@ -7,10 +7,10 @@
 from buildbot.plugins import steps, util
 from packaging.version import Version, parse
 
-factory_probe_basic_dev = util.BuildFactory()
+factory_probe_basic_py2 = util.BuildFactory()
 
 # fetch sources
-factory_probe_basic_dev.addStep(steps.GitHub(name="download probe_basic sources",
+factory_probe_basic_py2.addStep(steps.GitHub(name="download probe_basic sources",
                                              repourl='git@github.com:kcjengr/probe_basic.git',
                                              branch='python2_maintenance',
                                              mode='full',
@@ -18,31 +18,31 @@ factory_probe_basic_dev.addStep(steps.GitHub(name="download probe_basic sources"
                                              workdir="sources/"))
 
 # install qtpyvcp
-factory_probe_basic_dev.addStep(steps.ShellCommand(
+factory_probe_basic_py2.addStep(steps.ShellCommand(
     name="install qtpyvcp from pip",
     command=["python2.7", "-m", "pip", "install", "--upgrade", "qtpyvcp"],
     workdir="sources/"))
 
 # install sources
-factory_probe_basic_dev.addStep(steps.ShellCommand(
+factory_probe_basic_py2.addStep(steps.ShellCommand(
     name="install probe basic from sources",
     command=["python2.7", "-m", "pip", "install", "--upgrade", "."],
     workdir="sources/"))
 
 # build binaries and wheel for distribution
-factory_probe_basic_dev.addStep(steps.ShellCommand(
+factory_probe_basic_py2.addStep(steps.ShellCommand(
     name="build binaries and wheel for distribution",
     command=["python2.7", "setup.py", "bdist_wheel"],
     workdir="sources/"))
 
 # build source for distribution
-factory_probe_basic_dev.addStep(steps.ShellCommand(
+factory_probe_basic_py2.addStep(steps.ShellCommand(
     name="build source for distribution",
     command=["python2.7", "setup.py", "sdist"],
     workdir="sources/"))
 
 # get version from installed probe_basic package
-factory_probe_basic_dev.addStep(
+factory_probe_basic_py2.addStep(
     steps.SetPropertyFromCommand(
         name="obtain probe_basic version number",
         command=["python2.7",
@@ -51,7 +51,7 @@ factory_probe_basic_dev.addStep(
         workdir="sources/"))
 
 # add version and date to installer package file
-factory_probe_basic_dev.addStep(
+factory_probe_basic_py2.addStep(
     steps.ShellCommand(name="add version and date to installer package file",
                        command=["python2.7",
                                 "pb-installer/scripts/create_probe_basic_package_config.py",
@@ -61,7 +61,7 @@ factory_probe_basic_dev.addStep(
                        workdir="sources/"))
 
 # add version and date to installer config file
-factory_probe_basic_dev.addStep(
+factory_probe_basic_py2.addStep(
     steps.ShellCommand(name="add version, date and repo to installer config file",
                        command=["python2.7",
                                 "pb-installer/scripts/create_config.py",
@@ -72,34 +72,34 @@ factory_probe_basic_dev.addStep(
                        workdir="sources/"))
 
 # copy files to installer directories
-factory_probe_basic_dev.addStep(steps.CopyDirectory(name="copy probebasic builds to the installer core package",
+factory_probe_basic_py2.addStep(steps.CopyDirectory(name="copy probebasic builds to the installer core package",
                                                     src="sources/dist",
                                                     dest="sources/pb-installer/packages/com.probebasic.core/data/"))
 
 # sim files to installer directories
-factory_probe_basic_dev.addStep(steps.CopyDirectory(name="copy probebasic configs to the installer sim package",
+factory_probe_basic_py2.addStep(steps.CopyDirectory(name="copy probebasic configs to the installer sim package",
                                                     src="sources/config",
                                                     dest="sources/pb-installer/packages/com.probebasic.sim/data/"))
 
 # configure the installer
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="configure the installer",
+factory_probe_basic_py2.addStep(steps.ShellCommand(name="configure the installer",
                                                    command=["qmake"],
                                                    workdir="sources/pb-installer",
                                                    env={"QT_SELECT": "qt5",
                                                         "PB_VERSION": util.Property("probe_basic_dev_version")}))
 
 # build the installer
-factory_probe_basic_dev.addStep(steps.Compile(name="compile the installer",
+factory_probe_basic_py2.addStep(steps.Compile(name="compile the installer",
                                               command=["make"],
                                               workdir="sources/pb-installer",
                                               env={"QT_SELECT": "qt5"}))
 
 # copy the packages to repository
-factory_probe_basic_dev.addStep(steps.CopyDirectory(name="copy the packages to repository",
+factory_probe_basic_py2.addStep(steps.CopyDirectory(name="copy the packages to repository",
                                                     src="sources/pb-installer/repo",
                                                     dest="/home/buildbot/repo/pb-py2"))
 
-factory_probe_basic_dev.addStep(
+factory_probe_basic_py2.addStep(
     steps.ShellCommand(
         name="chmod repo directory",
         command=["sh", "-c", "chmod u+rx repo"],
@@ -108,12 +108,12 @@ factory_probe_basic_dev.addStep(
 )
 
 # copy the installer to repository
-factory_probe_basic_dev.addStep(steps.CopyDirectory(name="copy the installer to repository",
+factory_probe_basic_py2.addStep(steps.CopyDirectory(name="copy the installer to repository",
                                                     src="sources/pb-installer/bin",
                                                     dest="/home/buildbot/repo/pb-py2"))
 
 
-factory_probe_basic_dev.addStep(
+factory_probe_basic_py2.addStep(
     steps.ShellCommand(
         name="chmod bin directory",
         command=["sh", "-c", "chmod u+rx bin"],
@@ -121,31 +121,31 @@ factory_probe_basic_dev.addStep(
     )
 )
 
-factory_probe_basic_dev.addStep(steps.RemoveDirectory(name="delete copy of the local repo", dir="sources/pb-installer/repo"))
-factory_probe_basic_dev.addStep(steps.RemoveDirectory(name="delete build directory", dir="sources/build/"))
-factory_probe_basic_dev.addStep(steps.RemoveDirectory(name="delete dist directory", dir="sources/dist/"))
+factory_probe_basic_py2.addStep(steps.RemoveDirectory(name="delete copy of the local repo", dir="sources/pb-installer/repo"))
+factory_probe_basic_py2.addStep(steps.RemoveDirectory(name="delete build directory", dir="sources/build/"))
+factory_probe_basic_py2.addStep(steps.RemoveDirectory(name="delete dist directory", dir="sources/dist/"))
 #
 #
-# factory_probe_basic_dev.addStep(steps.GitHub(name="downlaod static docs",
+# factory_probe_basic_py2.addStep(steps.GitHub(name="downlaod static docs",
 #                                              repourl='git@github.com:kcjengr/probe_basic.git',
 #                                              origin="origin",
 #                                              branch="gh-pages",
 #                                              mode='full',
 #                                              workdir="docs/"))
 #
-# factory_probe_basic_dev.addStep(steps.ShellCommand(name="reset gh-pages",
+# factory_probe_basic_py2.addStep(steps.ShellCommand(name="reset gh-pages",
 #                                                    command=["git", "symbolic-ref", "HEAD", "refs/heads/gh-pages"],
 #                                                    workdir="docs/"))
 #
-# factory_probe_basic_dev.addStep(steps.ShellCommand(name="delete git index",
+# factory_probe_basic_py2.addStep(steps.ShellCommand(name="delete git index",
 #                                                    command=["rm", ".git/index"],
 #                                                    workdir="docs/"))
 #
-# factory_probe_basic_dev.addStep(steps.ShellCommand(name="clean gh-pages",
+# factory_probe_basic_py2.addStep(steps.ShellCommand(name="clean gh-pages",
 #                                                    command=["git", "clean", "-fdx"],
 #                                                    workdir="docs/"))
 #
-# factory_probe_basic_dev.addStep(
+# factory_probe_basic_py2.addStep(
 #     steps.Sphinx(
 #         name="compile sphinx docs",
 #         haltOnFailure=True,
@@ -156,6 +156,6 @@ factory_probe_basic_dev.addStep(steps.RemoveDirectory(name="delete dist director
 #         env={"LANG": "en_EN.UTF-8"},
 #         workdir="docs/"))
 #
-# factory_probe_basic_dev.addStep(steps.ShellCommand(name="add doc files", command=["git", "add", "."], workdir="docs/"))
-# factory_probe_basic_dev.addStep(steps.ShellCommand(name="commit doc files", command=["git", "commit", "-a", "-m", "deploy gh-pages"], workdir="docs/"))
-# factory_probe_basic_dev.addStep(steps.ShellCommand(name="push docs", command=["git", "push", "--force", "origin", "gh-pages"], workdir="docs/"))
+# factory_probe_basic_py2.addStep(steps.ShellCommand(name="add doc files", command=["git", "add", "."], workdir="docs/"))
+# factory_probe_basic_py2.addStep(steps.ShellCommand(name="commit doc files", command=["git", "commit", "-a", "-m", "deploy gh-pages"], workdir="docs/"))
+# factory_probe_basic_py2.addStep(steps.ShellCommand(name="push docs", command=["git", "push", "--force", "origin", "gh-pages"], workdir="docs/"))
