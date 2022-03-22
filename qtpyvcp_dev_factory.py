27a22fba417623ca218f724d8b44af7b61fe66e4
TurBoss <j.l.toledano.l@gmail.com>
2022-03-22 21:45:25 +0100

    enable testing debs

--- qtpyvcp_dev_factory.py
+++ qtpyvcp_dev_factory.py
@@ -37,26 +37,26 @@ factory_qtpyvcp_dev.addStep(steps.ShellCommand(
     workdir="sources/"))
 
 # build debian packages
-# factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-#    command=["fpm", "-t", "deb", "-p", "debs", "-s", "python", "-f", "--license", 'GPLv2',
-#            "--vendor", "KCJ Engineering",
-#             "--maintainer", "Kurt Jacobson <kcjengr@gmail.com>", "--url",
-#            "https://qtpyvcp.kcjengr.com" ,"--description",
-#            "QtPyVCP - Qt and Python based Virtual Control Panel framework for LinuxCNC.",
-#            "-d", "python-pip",
-#            "-d", "python-pyqt5",
-#            "-d", "python-dbus.mainloop.pyqt5",
-#            "-d", "python-pyqt5.qtopengl",
-#            "-d", "python-pyqt5.qsci",
-#            "-d", "python-pyqt5.qtmultimedia",
-#            "-d", "gstreamer1.0-plugins-bad",
-#            "-d", "libqt5multimedia5-plugins",
-#            "-d", "pyqt5-dev-tools",
-#            "-d", "qttools5-dev-tools",
-#            "--after-install", ".travis/after_install.sh",
-#            "--after-remove", ".travis/after_remove.sh",
-#            "--no-auto-depends",
-#            "--verbose", "setup.py"],))
+factory_qtpyvcp_dev.addStep(steps.ShellCommand(
+   command=["fpm", "-t", "deb", "-p", "debs", "-s", "python", "-f", "--license", 'GPLv2',
+           "--vendor", "KCJ Engineering",
+            "--maintainer", "Kurt Jacobson <kcjengr@gmail.com>", "--url",
+           "https://qtpyvcp.kcjengr.com" ,"--description",
+           "QtPyVCP - Qt and Python based Virtual Control Panel framework for LinuxCNC.",
+           "-d", "python-pip",
+           "-d", "python-pyqt5",
+           "-d", "python-dbus.mainloop.pyqt5",
+           "-d", "python-pyqt5.qtopengl",
+           "-d", "python-pyqt5.qsci",
+           "-d", "python-pyqt5.qtmultimedia",
+           "-d", "gstreamer1.0-plugins-bad",
+           "-d", "libqt5multimedia5-plugins",
+           "-d", "pyqt5-dev-tools",
+           "-d", "qttools5-dev-tools",
+           "--after-install", ".scripts/after_install.sh",
+           "--after-remove", ".scripts/after_remove.sh",
+           "--no-auto-depends",
+           "--verbose", "setup.py"],))
 
 
 # get version from installed qtpyvcp package
