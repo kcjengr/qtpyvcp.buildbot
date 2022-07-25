0668d83046251cda313860ba088509a8b55e3116
TurBoss <j.l.toledano.l@gmail.com>
2022-07-25 03:43:44 +0200

    add debs factory

---
+++ qtpyvcp_deb_dev_factory.py
@@ -0,0 +1,26 @@
+# -*- python3 -*-
+# ex: set syntax=python3:
+#
+# QtPyVCP Debian Develop Factory
+#
+
+from buildbot.plugins import steps, util
+from packaging.version import Version, parse
+
+factory_qtpyvcp_deb_dev = util.BuildFactory()
+
+# fetch sources
+factory_qtpyvcp_deb_dev.addStep(steps.GitHub(name="get sources",
+                                             repourl='git@github.com:kcjengr/probe_basic.git',
+                                             branch='python3',
+                                             mode='full',
+                                             submodules=False,
+                                             workdir="sources/"))
+
+
+
+# build debs
+factory_qtpyvcp_deb_dev.addStep(steps.ShellCommand(
+    name="build debs",
+    command=["DEB_BUILD_OPTIONS=nocheck", "dpkg-buildpackage", "-b", "-uc"],
+    workdir="sources/"))
