6582f073641d8d209c78dc85d4abf589574248e8
TurBoss <j.l.toledano.l@gmail.com>
2023-02-28 15:39:28 +0100

    fix qtpyvcp steps

--- qtpyvcp_dev_factory.py
+++ qtpyvcp_dev_factory.py
@@ -17,117 +17,12 @@ factory_qtpyvcp_dev.addStep(steps.GitHub(name="download qtpyvcp sources",
                                          submodules=True,
                                          workdir="sources/"))
 
-# install qtpyvcp to buildbot python
+# build debs
 factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-    name="install qtpyvcp from sources into buildbot python",
-    command=["python3", "-m", "pip", "install", "--upgrade", "-e", "."],
+    name="build debs",
+    env={'DEB_BUILD_OPTIONS': "nocheck"},
+    command=["dpkg-buildpackage", "-b", "-uc"],
     workdir="sources/"))
-
-
-# build binaries and wheel for distribution
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-    name="build binaries and wheel for distribution",
-    command=["python3", "setup.py", "bdist_wheel"],
-    workdir="sources/"))
-
-# build source for distribution
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-    name="build source for distribution",
-    command=["python3", "setup.py", "sdist"],
-    workdir="sources/"))
-
-"""
-# build debian packages
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-   command=["fpm", "-t", "deb", "-p", "debs", "-s", "python", "-f", "--license", 'GPLv2',
-           "--vendor", "KCJ Engineering",
-            "--maintainer", "Kurt Jacobson <kcjengr@gmail.com>", "--url",
-           "https://qtpyvcp.kcjengr.com" ,"--description",
-           "QtPyVCP - Qt and Python based Virtual Control Panel framework for LinuxCNC.",
-           "-d", "python3-pip",
-           "-d", "python3-pyqt5",
-           "-d", "python3-dbus.mainloop.pyqt5",
-           "-d", "python3-pyqt5.qtopengl",
-           "-d", "python3-pyqt5.qsci",
-           "-d", "python3-pyqt5.qtmultimedia",
-           "-d", "gstreamer1.0-plugins-bad",
-           "-d", "libqt5multimedia5-plugins",
-           "-d", "pyqt5-dev-tools",
-           "-d", "qttools5-dev-tools",
-           "--after-install", ".scripts/after_install.sh",
-           "--after-remove", ".scripts/after_remove.sh",
-           "--no-auto-depends",
-           "--verbose", "setup.py"],))
-"""
-
-# get version from installed qtpyvcp package
-factory_qtpyvcp_dev.addStep(
-    steps.SetPropertyFromCommand(
-        name="obtain qtpyvcp version number",
-        command=["python3",
-                 "installer/scripts/check_version.py"],
-        property="qtpyvcp_dev_version",
-        workdir="sources/"))
-
-# add version and date to installer config file
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-    name="add version and date to installer package file",
-    command=["python3",
-             "installer/scripts/create_config.py",
-             "installer/templates/config_template.xml",
-             "installer/config/config.xml",
-             "https://repository.qtpyvcp.com/repo/dev/repo/",
-             util.Property("qtpyvcp_dev_version")
-             ],
-    workdir="sources/"))
-
-# add version and date to installer package file
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-    name="add version and date to installer package file",
-    command=["python3",
-             "installer/scripts/create_package_config.py",
-             "installer/templates/package_template.xml",
-             "installer/packages/com.qtpyvcp.core/meta/package.xml",
-             util.Property("qtpyvcp_dev_version")
-             ],
-    workdir="sources/"))
-
-# copy files to installer directories
-factory_qtpyvcp_dev.addStep(steps.CopyDirectory(
-    name="copy qtpyvcp builds to the installer core package",
-    src="sources/dist",
-    dest="sources/installer/packages/com.qtpyvcp.core/data"))
-
-factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(dir="sources/dist"))
-factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(dir="sources/installer/repo"))
-
-# configure the installer
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="configure the installer",
-                                               command=["qmake"],
-                                               workdir="sources/installer",
-                                               env={"QT_SELECT": "qt5",
-                                                    "QTPYVCP_VERSION": util.Property("qtpyvcp_dev_version")}))
-
-# build the installer
-factory_qtpyvcp_dev.addStep(steps.Compile(name="compile the installer",
-                                          command=["make"],
-                                          workdir="sources/installer",
-                                          env={"QT_SELECT": "qt5"}))
-
-# copy packages to repository
-factory_qtpyvcp_dev.addStep(steps.CopyDirectory(name="copy the packages to repository",
-                                                src="sources/installer/repo",
-                                                dest="/home/buildbot/repo/dev"))
-
-# copy installer to repository
-factory_qtpyvcp_dev.addStep(steps.CopyDirectory(name="copy the installer to repository",
-                                                src="sources/installer/bin",
-                                                dest="/home/buildbot/repo/dev"))
-
-factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete copy of the local repo", dir="sources/installer/repo"))
-factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete build directory", dir="sources/build/"))
-factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete dist directory", dir="sources/dist/"))
-
 
 factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs/"))
 
