5726b16551bae08813f326f1291b874b715db62b
TurBoss <j.l.toledano.l@gmail.com>
2023-03-11 16:25:52 +0100

    fix probe-basic dev deb

--- probe_basic_dev_factory.py
+++ probe_basic_dev_factory.py
@@ -10,17 +10,38 @@ from packaging.version import Version, parse
 factory_probe_basic_dev = util.BuildFactory()
 
 
-# fetch sources
-factory_probe_basic_dev.addStep(steps.GitHub(name="download probe_basic sources",
+# download sources
+factory_probe_basic_dev.addStep(steps.GitHub(name="download sources",
                                              repourl='git@github.com:kcjengr/probe_basic.git',
                                              branch='main',
                                              mode='full',
                                              submodules=False,
                                              workdir="sources/"))
+# get git tag
+factory_probe_basic_dev.addStep(steps.SetPropertyFromCommand(
+    name="get git tag",
+    command=["git", "describe", "--abbrev=0", "--tags"],
+    property="tag",
+    workdir="sources/"))
 
+# get git commit ID
+factory_probe_basic_dev.addStep(steps.SetPropertyFromCommand(
+    name="get git commit ID",
+    command=["git", "rev-parse", "--short","HEAD"],
+    property="commit_id",
+    workdir="sources/"))
+
+# compile resources
 factory_probe_basic_dev.addStep(steps.ShellCommand(
     name="compile resources",
     command=["qcompile", "."],
+    workdir="sources/"))
+
+# create changelog
+factory_probe_basic_dev.addStep(steps.ShellCommand(
+    name="create changelog",
+    env={'EMAIL': "j.l.toledano.l@gmail.com"},
+    command=["dch", "--create", "--distribution", "unstable", "--package", "probe-basic", "--newversion", util.Interpolate("%(prop:tag)s-%(prop:commit_id)s.dev"), "Development version."],
     workdir="sources/"))
 
 # build debs
@@ -30,6 +51,13 @@ factory_probe_basic_dev.addStep(steps.ShellCommand(
     command=["dpkg-buildpackage", "-b", "-uc"],
     workdir="sources/"))
 
+# move files to repo
+factory_probe_basic_dev.addStep(steps.ShellCommand(
+    name="move files to repo",
+    command=["mv",
+             util.Interpolate("/home/buildbot/buildbot/worker/probe-basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb"),
+             "/home/buildbot/repo/probe-basic-dev/"],
+    workdir="sources/"))
 
 # factory_probe_basic_dev.addStep(steps.GitHub(name="downlaod static docs",
 #                                              repourl='git@github.com:kcjengr/probe_basic.git',
