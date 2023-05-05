9515bcc5c3f3de5e4d35f479f9b9f246b4e73e42
TurBoss <j.l.toledano.l@gmail.com>
2023-05-05 16:57:00 +0200

    add steps to populate apt repos

--- qtpyvcp_dev_factory.py
+++ qtpyvcp_dev_factory.py
@@ -52,49 +52,81 @@ factory_qtpyvcp_dev.addStep(steps.ShellCommand(
     command=["dpkg-buildpackage", "-b", "-uc"],
     workdir="sources/"))
 
-# move files to repo
+# copy files to the http repo
 factory_qtpyvcp_dev.addStep(steps.ShellCommand(
-    name="move files to repo",
-    command=["mv",
+    name="copy files to the http repo",
+    command=["cp",
              util.Interpolate("/home/buildbot/buildbot/worker/qtpyvcp-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb"),
              "/home/buildbot/repo/qtpyvcp-dev/"],
     workdir="sources/"))
 
-factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs/"))
+# copy files to the apt repo
+factory_qtpyvcp_dev.addStep(steps.ShellCommand(
+    name="copy files to repo",
+    command=["cp",
+             util.Interpolate("/home/buildbot/buildbot/worker/qtpyvcp-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb"),
+             "/home/buildbot/debian/apt/pool/main/"],
+    workdir="sources/"))
 
-factory_qtpyvcp_dev.addStep(
-    steps.Sphinx(
-        name="compile sphinx docs",
-        haltOnFailure=True,
-        sphinx="/home/buildbot/venv/bin/sphinx-build",
-        sphinx_builddir="/home/buildbot/buildbot/worker/qtpyvcp-dev/docs",
-        sphinx_sourcedir="/home/buildbot/buildbot/worker/qtpyvcp-dev/sources/docs/source/",
-        strict_warnings=False,
-        env={"LANG": "en_EN.UTF-8"},
-        workdir="sources/docs/source/"
-    )
-)
+# delete files from build directory
+factory_qtpyvcp_dev.addStep(steps.ShellCommand(
+    name="delete files from build directory",
+    command=["rm", util.Interpolate("/home/buildbot/buildbot/worker/qtpyvcp-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb")],
+    workdir="sources/"))
 
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
-                                               command=["git", "init"],
-                                               workdir="docs/"))
+# scan new packages in apt repository
+factory_qtpyvcp_dev.addStep(steps.ShellCommand(
+    name="scan new packages in apt repository",
+    command=["dpkg-scanpackages", "--arch", "amd64", "pool", ">", "dists/develop/main/binary-amd64/Packages"],
+    workdir="/home/buildbot/debian/apt/"))
 
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="add remote repository",
-                                               command=["git", "remote", "add", "origin", "git@github.com:kcjengr/qtpyvcp.git"],
-                                               workdir="docs/"))
+# gzip package list
+factory_qtpyvcp_dev.addStep(steps.ShellCommand(
+    name="gzip package list",
+    command=["cat", "dists/develop/main/binary-amd64/Packages", "|", "gzip", "-9", ">", "dists/develop/main/binary-amd64/Packages.gz"],
+    workdir="/home/buildbot/debian/apt/"))
 
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="switch branch",
-                                               command=["git", "checkout", "-b", "gh-pages"],
-                                               workdir="docs/"))
 
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="add docs",
-                                               command=["git", "add", "."],
-                                               workdir="docs/"))
 
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="commit docs",
-                                               command=["git", "commit", "-m", "Deploy docs"],
-                                               workdir="docs/"))
 
-factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="push docs",
-                                               command=["git", "push", "--force", "origin", "gh-pages"],
-                                               workdir="docs/"))
+
+
+# delete docs directory
+# factory_qtpyvcp_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs/"))
+#
+# factory_qtpyvcp_dev.addStep(
+#     steps.Sphinx(
+#         name="compile sphinx docs",
+#         haltOnFailure=True,
+#         sphinx="/home/buildbot/venv/bin/sphinx-build",
+#         sphinx_builddir="/home/buildbot/buildbot/worker/qtpyvcp-dev/docs",
+#         sphinx_sourcedir="/home/buildbot/buildbot/worker/qtpyvcp-dev/sources/docs/source/",
+#         strict_warnings=False,
+#         env={"LANG": "en_EN.UTF-8"},
+#         workdir="sources/docs/source/"
+#     )
+# )
+#
+# factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
+#                                                command=["git", "init"],
+#                                                workdir="docs/"))
+#
+# factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="add remote repository",
+#                                                command=["git", "remote", "add", "origin", "git@github.com:kcjengr/qtpyvcp.git"],
+#                                                workdir="docs/"))
+#
+# factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="switch branch",
+#                                                command=["git", "checkout", "-b", "gh-pages"],
+#                                                workdir="docs/"))
+#
+# factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="add docs",
+#                                                command=["git", "add", "."],
+#                                                workdir="docs/"))
+#
+# factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="commit docs",
+#                                                command=["git", "commit", "-m", "Deploy docs"],
+#                                                workdir="docs/"))
+#
+# factory_qtpyvcp_dev.addStep(steps.ShellCommand(name="push docs",
+#                                                command=["git", "push", "--force", "origin", "gh-pages"],
+#                                                workdir="docs/"))
