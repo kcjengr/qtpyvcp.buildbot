423cd1c2be87a78e91b75bc798517ca9fe25c7e9
TurBoss <j.l.toledano.l@gmail.com>
2023-05-06 15:43:32 +0200

    update repo with probe_basic packages

--- probe_basic_dev_factory.py
+++ probe_basic_dev_factory.py
@@ -52,46 +52,71 @@ factory_probe_basic_dev.addStep(steps.ShellCommand(
     command=["dpkg-buildpackage", "-b", "-uc"],
     workdir="sources/"))
 
-# move files to repo
+# copy files to the http repo
 factory_probe_basic_dev.addStep(steps.ShellCommand(
-    name="move files to repo",
-    command=["mv",
+    name="copy files to the http repo",
+    command=["cp",
              util.Interpolate("/home/buildbot/buildbot/worker/probe_basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb"),
              "/home/buildbot/repo/probe-basic-dev/"],
     workdir="sources/"))
 
 
+# delete old files from apt directory
+factory_probe_basic_dev.addStep(steps.ShellCommand(
+    name="delete files from build directory",
+    command=["sh", "clean_apt_develop.sh"],
+    workdir="sources/.scripts/"))
 
-factory_probe_basic_dev.addStep(steps.GitHub(name="downlaod static docs",
-                                             repourl='git@github.com:kcjengr/probe_basic.git',
-                                             origin="origin",
-                                             branch="gh-pages",
-                                             mode='full',
-                                             workdir="docs/"))
+# copy new files to the apt repo
+factory_probe_basic_dev.addStep(steps.ShellCommand(
+    name="copy files to repo",
+    command=["cp",
+             util.Interpolate("/home/buildbot/buildbot/worker/probe_basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb"),
+             "/home/buildbot/debian/apt/pool/main/"],
+    workdir="sources/"))
 
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="reset gh-pages",
-                                                   command=["git", "symbolic-ref", "HEAD", "refs/heads/gh-pages"],
-                                                   workdir="docs/"))
+# delete files from build directory
+factory_probe_basic_dev.addStep(steps.ShellCommand(
+    name="delete files from build directory",
+    command=["rm", util.Interpolate("/home/buildbot/buildbot/worker/probe_basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:commit_id)s.dev_all.deb")],
+    workdir="sources/"))
 
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="delete git index",
-                                                   command=["rm", ".git/index"],
-                                                   workdir="docs/"))
+# scan new packages in apt repository
+factory_probe_basic_dev.addStep(steps.ShellCommand(
+    name="scan new packages in apt repository",
+    command=["sh", "do_apt_develop.sh"],
+    workdir="sources/.scripts/"))
 
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="clean gh-pages",
-                                                   command=["git", "clean", "-fdx"],
-                                                   workdir="docs/"))
-
-factory_probe_basic_dev.addStep(
-    steps.Sphinx(
-        name="compile sphinx docs",
-        haltOnFailure=True,
-        sphinx="/home/buildbot/venv/bin/sphinx-build",
-        sphinx_builddir="/home/buildbot/buildbot/worker/probe_basic-dev/docs/",
-        sphinx_sourcedir="/home/buildbot/buildbot/worker/probe_basic-dev/sources/docs_src/source/",
-        strict_warnings=False,
-        env={"LANG": "en_EN.UTF-8"},
-        workdir="docs/"))
-
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="add doc files", command=["git", "add", "."], workdir="docs/"))
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="commit doc files", command=["git", "commit", "-a", "-m", "deploy gh-pages"], workdir="docs/"))
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="push docs", command=["git", "push", "--force", "origin", "gh-pages"], workdir="docs/"))
+# factory_probe_basic_dev.addStep(steps.GitHub(name="downlaod static docs",
+#                                              repourl='git@github.com:kcjengr/probe_basic.git',
+#                                              origin="origin",
+#                                              branch="gh-pages",
+#                                              mode='full',
+#                                              workdir="docs/"))
+#
+# factory_probe_basic_dev.addStep(steps.ShellCommand(name="reset gh-pages",
+#                                                    command=["git", "symbolic-ref", "HEAD", "refs/heads/gh-pages"],
+#                                                    workdir="docs/"))
+#
+# factory_probe_basic_dev.addStep(steps.ShellCommand(name="delete git index",
+#                                                    command=["rm", ".git/index"],
+#                                                    workdir="docs/"))
+#
+# factory_probe_basic_dev.addStep(steps.ShellCommand(name="clean gh-pages",
+#                                                    command=["git", "clean", "-fdx"],
+#                                                    workdir="docs/"))
+#
+# factory_probe_basic_dev.addStep(
+#     steps.Sphinx(
+#         name="compile sphinx docs",
+#         haltOnFailure=True,
+#         sphinx="/home/buildbot/venv/bin/sphinx-build",
+#         sphinx_builddir="/home/buildbot/buildbot/worker/probe_basic-dev/docs/",
+#         sphinx_sourcedir="/home/buildbot/buildbot/worker/probe_basic-dev/sources/docs_src/source/",
+#         strict_warnings=False,
+#         env={"LANG": "en_EN.UTF-8"},
+#         workdir="docs/"))
+#
+# factory_probe_basic_dev.addStep(steps.ShellCommand(name="add doc files", command=["git", "add", "."], workdir="docs/"))
+# factory_probe_basic_dev.addStep(steps.ShellCommand(name="commit doc files", command=["git", "commit", "-a", "-m", "deploy gh-pages"], workdir="docs/"))
+# factory_probe_basic_dev.addStep(steps.ShellCommand(name="push docs", command=["git", "push", "--force", "origin", "gh-pages"], workdir="docs/"))
