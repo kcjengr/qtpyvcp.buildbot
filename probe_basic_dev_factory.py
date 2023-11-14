155cdad82bb54a046198ea4970a998aee0ebdc13
TurBoss <j.l.toledano.l@gmail.com>
2023-11-14 21:36:57 +0100

    copy sphinx steps from qtpyvcp to probe_basic to get rid of gh-pages git history

--- probe_basic_dev_factory.py
+++ probe_basic_dev_factory.py
@@ -97,36 +97,42 @@ factory_probe_basic_dev.addStep(steps.ShellCommand(
     command=["sh", "/home/buildbot/buildbot/master/scripts/do_apt_develop.sh"],
     workdir="sources/"))
 
-factory_probe_basic_dev.addStep(steps.GitHub(name="downlaod static docs",
-                                             repourl='git@github.com:kcjengr/probe_basic.git',
-                                             origin="origin",
-                                             branch="gh-pages",
-                                             mode='full',
-                                             workdir="docs/"))
-
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="reset gh-pages",
-                                                   command=["git", "symbolic-ref", "HEAD", "refs/heads/gh-pages"],
-                                                   workdir="docs/"))
-
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="delete git index",
-                                                   command=["rm", ".git/index"],
-                                                   workdir="docs/"))
-
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="clean gh-pages",
-                                                   command=["git", "clean", "-fdx"],
-                                                   workdir="docs/"))
+# delete docs directory
+factory_probe_basic_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs_src/"))
 
 factory_probe_basic_dev.addStep(
     steps.Sphinx(
         name="compile sphinx docs",
         haltOnFailure=True,
         sphinx="/home/buildbot/venv/bin/sphinx-build",
-        sphinx_builddir="/home/buildbot/buildbot/worker/probe_basic-dev/docs/",
-        sphinx_sourcedir="/home/buildbot/buildbot/worker/probe_basic-dev/sources/docs_src/source/",
+        sphinx_builddir="/home/buildbot/buildbot/worker/qtpyvcp-dev/docs_src",
+        sphinx_sourcedir="/home/buildbot/buildbot/worker/qtpyvcp-dev/sources/docs_src/source/",
         strict_warnings=False,
         env={"LANG": "en_EN.UTF-8"},
-        workdir="docs/"))
+        workdir="sources/docs_src/source/"
+    )
+)
 
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="add doc files", command=["git", "add", "."], workdir="docs/"))
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="commit doc files", command=["git", "commit", "-a", "-m", "deploy gh-pages"], workdir="docs/"))
-factory_probe_basic_dev.addStep(steps.ShellCommand(name="push docs", command=["git", "push", "--force", "origin", "gh-pages"], workdir="docs/"))
+factory_probe_basic_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
+                                               command=["git", "init"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_dev.addStep(steps.ShellCommand(name="add remote repository",
+                                               command=["git", "remote", "add", "origin", "git@github.com:kcjengr/probe_basic.git"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_dev.addStep(steps.ShellCommand(name="switch branch",
+                                               command=["git", "checkout", "-b", "gh-pages"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_dev.addStep(steps.ShellCommand(name="add docs",
+                                               command=["git", "add", "."],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_dev.addStep(steps.ShellCommand(name="commit docs",
+                                               command=["git", "commit", "-m", "Deploy docs"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_dev.addStep(steps.ShellCommand(name="push docs",
+                                               command=["git", "push", "--force", "origin", "gh-pages"],
+                                               workdir="docs_src/"))
