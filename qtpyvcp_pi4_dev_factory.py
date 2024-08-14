c42e57f26882613b739956fce03be3e192686353
joco-nz <james.walker.nz>
2024-08-14 17:51:37 +1200

    Update qtpyvcp_pi4_dev_factory.py - fix missing ) and #

--- qtpyvcp_pi4_dev_factory.py
+++ qtpyvcp_pi4_dev_factory.py
@@ -55,19 +55,19 @@ factory_qtpyvcp_pi4_dev.addStep(steps.ShellCommand(
 factory_qtpyvcp_pi4_dev.addStep(steps.FileUpload(
     name="copy files to the http repo",
     workersrc=util.Interpolate("/home/buildbot/workdir/qtpyvcp-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_all.deb"),
-    masterdest="/home/buildbot/repo/qtpyvcp-dev/")
+    masterdest="/home/buildbot/repo/qtpyvcp-dev/"))
 
 
 # copy new files to the apt repo on .13 machine
 factory_qtpyvcp_pi4_dev.addStep(steps.FileUpload(
     name="copy files to repo",
     workersrc=util.Interpolate("/home/buildbot/workdir/qtpyvcp-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_all.deb"),
-    masterdest="/home/buildbot/debian/apt/pool/main/develop/")
+    masterdest="/home/buildbot/debian/apt/pool/main/develop/"))
 
 
-scan new packages in apt repository
+#scan new packages in apt repository
 factory_qtpyvcp_pi4_dev.addStep(steps.MasterShellCommand(
     name="scan new packages in apt repository",
     command="/home/buildbot/buildbot/master/scripts/do_apt_develop.sh",
-    workdir="sources/")
+    workdir="sources/"))
 
