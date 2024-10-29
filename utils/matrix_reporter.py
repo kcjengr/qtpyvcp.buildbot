00b79e6a0875bf8c58cc4af84e24171c88a1a144
TurBoss <j.l.toledano.l@gmail.com>
2024-10-29 07:39:38 +0100

    fix params

--- utils/matrix_reporter.py
+++ utils/matrix_reporter.py
@@ -10,29 +10,45 @@ class MatrixReporter(ReporterBase):
     name = "MatrixReporter"
     secrets = []
 
+    server_url = ""
+    user_name = ""
+    user_pass = ""
     room_id = ""
+    debug = ""
 
-
-    def checkConfig(self, serverUrl, userName=None, auth=None, roomID=None, headers=None,
+    def checkConfig(self, serverUrl, userName=None, userPass=None, roomID=None, headers=None,
                     debug=None, verify=None, generators=None, **kwargs):
 
         if generators is None:
             generators = self._create_default_generators()
+
+        self.server_url = serverUrl
+        self.user_name = userName
+        self.user_pass = userPass
         self.room_id = roomID
+        self.debug = debug
+
         super().checkConfig(generators=generators, **kwargs)
 
     @defer.inlineCallbacks
-    def reconfigService(self, serverUrl, userName=None, auth=None, roomID=None, headers=None,
+    def reconfigService(self, serverUrl, userName=None, userPass=None, roomID=None, headers=None,
                     debug=None, verify=None, generators=None, **kwargs):
         self.debug = debug
         self.verify = verify
+
+        self.server_url = serverUrl
+        self.user_name = userName
+        self.user_pass = userPass
+        self.room_id = roomID
+        self.debug = debug
+
 
         if generators is None:
             generators = self._create_default_generators()
 
         yield super().reconfigService(generators=generators, **kwargs)
 
-        self._client = AsyncClient(serverUrl, userName)
+        self._client = AsyncClient(self.server_url, self.user_name)
 
     def _create_default_generators(self):
         formatter = MessageFormatterFunction(lambda context: context['build'], 'plain')
@@ -43,6 +59,6 @@ class MatrixReporter(ReporterBase):
     @defer.inlineCallbacks
     def sendMessage(self, reports):
         msg_text = reports[0]['body']
-        self._client.login(matrix_pass)
+        self._client.login(self.matrix_pass)
         yield self._client.room_send(room_id=self.room_id, message_type="m.room.message",
                                      content={"msgtype":"m.text", "body":msg_text})
