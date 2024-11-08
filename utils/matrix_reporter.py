00646b37bd972a4d8cd7dcc3f160ee8b53b4c40d
TurBoss <j.l.toledano.l@gmail.com>
2024-11-08 08:21:54 +0100

    back to origins

--- utils/matrix_reporter.py
+++ utils/matrix_reporter.py
@@ -1,154 +1,119 @@
 import asyncio
 import logging
 
-from twisted.internet import defer, reactor
+from datetime import datetime
+from nio import AsyncClient, MatrixRoom, RoomMessageText
+
+from twisted.internet import defer
 from buildbot.reporters.generators.build import BuildStatusGenerator
 from buildbot.reporters.message import MessageFormatterFunction, MessageFormatter
 from buildbot.reporters.base import ReporterBase
 from buildbot.reporters.utils import merge_reports_prop, merge_reports_prop_take_first
-from nio import AsyncClient, RoomSendResponse, RoomSendError
-
-from twisted.internet import defer
-from twisted.internet import reactor
-
-from buildbot import config
-from buildbot.process.results import CANCELLED
-from buildbot.process.results import EXCEPTION
-from buildbot.process.results import FAILURE
-from buildbot.process.results import RETRY
-from buildbot.process.results import SUCCESS
-from buildbot.process.results import WARNINGS
-from buildbot.reporters.words import Channel
-from buildbot.reporters.words import Contact
-from buildbot.reporters.words import StatusBot
-from buildbot.reporters.words import UsageError
-from buildbot.reporters.words import WebhookResource
-from buildbot.schedulers.forcesched import CollectedValidationError
-from buildbot.schedulers.forcesched import ForceScheduler
-from buildbot.util import Notifier
-from buildbot.util import asyncSleep
-from buildbot.util import bytes2unicode
-from buildbot.util import epoch2datetime
-from buildbot.util import httpclientservice
-from buildbot.util import service
-from buildbot.util import unicode2bytes
 
 # Enable debug logging for the nio client
-
 logging.basicConfig(level=logging.DEBUG)
 
-class MatrixChannel:
-    def __init__(self, bot, room_id):
-        self.bot = bot
-        self.room_id = room_id
+
+class MatrixReporter(ReporterBase):
+    name = "MatrixReporter"
+    secrets = []
+
+    server_url = ""
+    user_name = ""
+    user_pass = ""
+    room_id = ""
+    debug = False
+
+    def checkConfig(self, serverUrl, userName=None, userToken=None, roomID=None, headers=None,
+                        debug=None, verify=None, generators=None, **kwargs):
+
+        if generators is None:
+            generators = self._create_default_generators()
+
+        self.server_url = serverUrl
+        self.user_name = userName
+        self.user_token = userToken
+        self.room_id = roomID
+        self.debug = debug
+
+        super().checkConfig(generators=generators, **kwargs)
 
     @defer.inlineCallbacks
-    def list_notified_events(self):
-        if self.bot.notify_events:
-            notified_events = "\n".join(sorted(f"🔔 **{n}**" for n in self.bot.notify_events))
-            yield self.bot.send_message(self.room_id, f"The following events are being notified:\n{notified_events}")
-        else:
-            yield self.bot.send_message(self.room_id, "🔕 No events are being notified.")
+    def reconfigService(self, serverUrl, userName=None, userToken=None, roomID=None, headers=None,
+                        debug=None, verify=None, generators=None, **kwargs):
+        self.debug = debug
+        self.verify = verify
 
-class MatrixContact:
-    def __init__(self, user_id, channel=None):
-        self.user_id = user_id
-        self.channel = channel
-        self.template = None
+        self.server_url = serverUrl
+        self.user_name = userName
+        self.user_token = userToken
+        self.room_id = roomID
+        self.debug = debug
 
-    @property
-    def chat_id(self):
-        return self.channel.room_id
+        if generators is None:
+            generators = self._create_default_generators()
 
-    def describeUser(self):
-        return f"User ID: {self.user_id}"
+        yield super().reconfigService(generators=generators, **kwargs)
 
-    @defer.inlineCallbacks
-    def command_START(self, args, **kwargs):
-        yield self.bot.send_message(self.channel.room_id, "Hello! How can I assist you?")
+        self._client = AsyncClient(self.server_url, self.user_name)
 
-    @defer.inlineCallbacks
-    def command_GETID(self, args, **kwargs):
-        yield self.bot.send_message(self.channel.room_id, f"Your ID is `{self.user_id}`.")
+        self._client.access_token = self.user_token
+        self._client.user_id = self.user_name
+        self._client.device_id = "buildbot"
 
-    @defer.inlineCallbacks
-    def command_HELP(self, args, **kwargs):
-        help_text = "Available commands:\n"
-        help_text += "/start - Start the bot\n"
-        help_text += "/getid - Get user ID\n"
-        help_text += "/help - Show this help message"
-        yield self.bot.send_message(self.channel.room_id, help_text)
+        # yield self._client.login()
 
-class MatrixStatusBot(ReporterBase):
-    name = "MatrixStatusBot"
+    def _create_default_generators(self):
 
-    def __init__(self, homeserver, user_id, access_token, room_id, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.homeserver = homeserver
-        self.user_id = user_id
-        self.access_token = access_token
-        self.room_id = room_id
-        self.client = AsyncClient(self.homeserver, self.user_id)
-        self.client.access_token = self.access_token
-        self.client.user_id = self.user_id
-        self.client.device_id = "buildbot"
+        BuildStatusGenerator(
+                add_patch=True, message_formatter=MessageFormatter(template_type='html')
+        ),
+        formatter = MessageFormatterFunction(lambda context: context['build'], 'plain')
+        return [BuildStatusGenerator(message_formatter=formatter, report_new=True)]
 
     @defer.inlineCallbacks
-    def startService(self):
-        yield super().startService()
-        self.channel = MatrixChannel(self, self.room_id)
-        self.contact = MatrixContact(self.user_id, self.channel)
+    def sendMessage(self, reports):
+        body = merge_reports_prop(reports, 'body')
+        subject = merge_reports_prop_take_first(reports, 'subject')
+        type = merge_reports_prop_take_first(reports, 'type')
+        results = merge_reports_prop(reports, 'results')
+        builds = merge_reports_prop(reports, 'builds')
+        users = merge_reports_prop(reports, 'users')
+        patches = merge_reports_prop(reports, 'patches')
+        logs = merge_reports_prop(reports, 'logs')
+        worker = merge_reports_prop_take_first(reports, 'worker')
 
-    @defer.inlineCallbacks
-    def send_message(self, room_id, message):
-        loop = asyncio.get_event_loop()
-        task = loop.create_task(self._send_message(room_id, message))
-        yield defer.Deferred.fromCoroutine(task)
+        print("#########################")
+        print("BODY @@@@@@@@@@@@@@@")
+        print(body)
+        print("SUBJECT @@@@@@@@@@@@@@@")
+        print(subject)
+        print("TYPE @@@@@@@@@@@@@@@")
+        print(type)
+        print("RESULTS @@@@@@@@@@@@@@@")
+        print(results)
+        print("BUILD @@@@@@@@@@@@@@@")
+        print(builds)
+        print("USERS @@@@@@@@@@@@@@@")
+        print(users)
+        print("PATCHES @@@@@@@@@@@@@@@")
+        print(patches)
+        print("LOGS @@@@@@@@@@@@@@@")
+        print(logs)
+        print("WORKER @@@@@@@@@@@@@@@")
+        print(worker)
+        print("#########################")
 
-    async def _send_message(self, room_id, message):
-        try:
-            response = await self.client.room_send(
-                room_id=room_id,
+        event_loop = asyncio.get_event_loop()
+
+        yield send(subject)
+
+    def send(self, subject, **kwargs):
+        asyncio.ensure_future(
+            self._client.room_send(
+                room_id=self.room_id,
                 message_type="m.room.message",
-                content={
-                    "msgtype": "m.text",
-                    "body": message,
-                },
-            )
-            if isinstance(response, RoomSendResponse):
-                print("Message sent successfully!")
-            elif isinstance(response, RoomSendError):
-                print(f"Failed to send message: {response.message}")
-        finally:
-            await self.client.close()
-
-    @defer.inlineCallbacks
-    def process_update(self, update):
-        message = update.get('content', {}).get('body')
-        if not message:
-            return 'no text in the message'
-
-        contact = self.contact
-        if message.startswith('/'):
-            command = message.split()[0]
-            handler = getattr(contact, 'command_' + command[1:].upper(), None)
-            if handler:
-                yield handler(message.split()[1:])
-            else:
-                yield self.send_message(self.room_id, "Unknown command. Type /help for available commands.")
-
-class MatrixBot(service.BuildbotService):
-    name = "MatrixBot"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.bot = None
-
-    @defer.inlineCallbacks
-    def reconfigService(self, homeserver, user_id, access_token, room_id, **kwargs):
-        if self.bot is not None:
-            self.removeService(self.bot)
-
-        self.bot = MatrixStatusBot(homeserver, user_id, access_token, room_id, **kwargs)
-        yield self.bot.setServiceParent(self)
-
+                content={"msgtype": "m.text", "body": f"Worker {worker} subject {subject}"}
+            ),
+            loop=event_loop
+        )
