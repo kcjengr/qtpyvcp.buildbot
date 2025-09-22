c3a832f971297e771b2d0f646a410ecafce1d741
TurBoss <j.l.toledano.l@gmail.com>
2025-09-22 03:53:28 +0200

    change to git to https

--- tnc_factory/arm64/stable/tnc_pyside6_arm64_factory.py
+++ tnc_factory/arm64/stable/tnc_pyside6_arm64_factory.py
@@ -11,7 +11,7 @@ factory_tnc_pyside6_arm64 = util.BuildFactory()
 
 # download sources
 factory_tnc_pyside6_arm64.addStep(steps.Git(name="download sources",
-                                            repourl='git@github.com:kcjengr/turbonc.git',
+                                            repourl='https://github.com/kcjengr/turbonc.git',
                                             mode='full',
                                             method="clean",
                                             tags=True,
