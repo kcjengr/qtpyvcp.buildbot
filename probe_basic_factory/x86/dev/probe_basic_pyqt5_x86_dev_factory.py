b24abf6485edd6f1801a63d08897c39c59f32b46
TurBoss <j.l.toledano.l@gmail.com>
2025-10-13 02:08:49 +0200

    change from ssh to https for pb repos

--- probe_basic_factory/x86/dev/probe_basic_pyqt5_x86_dev_factory.py
+++ probe_basic_factory/x86/dev/probe_basic_pyqt5_x86_dev_factory.py
@@ -12,7 +12,7 @@ factory_probe_basic_pyqt5_x86_dev = util.BuildFactory()
 
 # download sources
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.GitHub(name="download sources",
-                                             repourl='git@github.com:kcjengr/probe_basic.git',
+                                             repourl='https://github.com/kcjengr/probe_basic.git',
                                              branch='main',
                                              mode='full',
                                              submodules=False,
