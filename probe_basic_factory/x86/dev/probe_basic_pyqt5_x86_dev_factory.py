fadac704611d1f96128d75e19f95a967b06ceccb
TurBoss <j.l.toledano.l@gmail.com>
2025-12-11 07:55:21 +0100

    enable building probe_basic docs

--- probe_basic_factory/x86/dev/probe_basic_pyqt5_x86_dev_factory.py
+++ probe_basic_factory/x86/dev/probe_basic_pyqt5_x86_dev_factory.py
@@ -39,19 +39,6 @@ factory_probe_basic_pyqt5_x86_dev.addStep(steps.SetPropertyFromCommand(
     property="minor_version",
     workdir="sources/"))
 
-# # store version file
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
-#     name="store version file",
-#     command=["/bin/sh", "-c", util.Interpolate('echo %(prop:tag)s-%(prop:minor_version)s > pb_dev_version.txt')],
-#     workdir="/home/buildbot/versions/"))
-
-# compile resources
-# disabled, done in deb build step
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
-#     name="compile resources",
-#     command=["qcompile", "."],
-#     workdir="sources/"))
-
 # create changelog
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
     name="create changelog",
@@ -87,80 +74,43 @@ factory_probe_basic_pyqt5_x86_dev.addStep(steps.MasterShellCommand(
     workdir="/home/buildbot/debian/apt"))
 
 
-# copy files to the http repo
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
-#     name="copy files to the http repo",
-#     command=["cp",
-#              util.Interpolate("/home/buildbot/buildbot/worker/probe_basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
-#              "/home/buildbot/repo/probe-basic-dev/"],
-#     workdir="sources/"))
-#
-#
-# # delete old files from apt directory
-# # factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
-# #     name="delete files from apt directory",
-# #     command=["sh",
-# #              "/home/buildbot/buildbot/master/scripts/clean_apt_develop.sh",
-# #              util.Interpolate("python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_all.deb")
-# #             ],
-# #     workdir="sources/"))
-#
-# # copy new files to the apt repo
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
-#     name="copy files to repo",
-#     command=["cp",
-#              util.Interpolate("/home/buildbot/buildbot/worker/probe_basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
-#              "/home/buildbot/debian/apt/pool/main/develop/"],
-#     workdir="sources/"))
-#
-# # delete files from build directory
-# # factory_probe_basic_dev.addStep(steps.ShellCommand(
-# #     name="delete files from build directory",
-# #     command=["rm", util.Interpolate("/home/buildbot/buildbot/worker/probe_basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_all.deb")],
-# #     workdir="sources/"))
-#
-# # scan new packages in apt repository
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
-#     name="scan new packages in apt repository",
-#     command=["sh", "/home/buildbot/buildbot/master/scripts/do_apt_develop.sh"],
-#     workdir="sources/"))
-#
-# # delete docs directory
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs_src/"))
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(
-#     steps.Sphinx(
-#         name="compile sphinx docs",
-#         haltOnFailure=True,
-#         sphinx="/home/buildbot/venv/bin/sphinx-build",
-#         sphinx_builddir="/home/buildbot/buildbot/worker/probe_basic-dev/docs_src",
-#         sphinx_sourcedir="/home/buildbot/buildbot/worker/probe_basic-dev/sources/docs_src/source/",
-#         strict_warnings=False,
-#         env={"LANG": "en_EN.UTF-8"},
-#         workdir="sources/docs_src/source/"
-#     )
-# )
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
-#                                                command=["git", "init"],
-#                                                workdir="docs_src/"))
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="add remote repository",
-#                                                command=["git", "remote", "add", "origin", "git@github.com:kcjengr/probe_basic.git"],
-#                                                workdir="docs_src/"))
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="switch branch",
-#                                                command=["git", "checkout", "-b", "gh-pages"],
-#                                                workdir="docs_src/"))
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="add docs",
-#                                                command=["git", "add", "."],
-#                                                workdir="docs_src/"))
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="commit docs",
-#                                                command=["git", "commit", "-m", "Deploy docs"],
-#                                                workdir="docs_src/"))
-#
-# factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="push docs",
-#                                                command=["git", "push", "--force", "origin", "gh-pages"],
-#                                                workdir="docs_src/"))
+
+# delete docs directory
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs_src/"))
+
+factory_probe_basic_pyqt5_x86_dev.addStep(
+    steps.Sphinx(
+        name="compile sphinx docs",
+        haltOnFailure=True,
+        sphinx="/home/bb/venv/bin/sphinx-build",
+        sphinx_builddir="/home/bb/buildbot/worker/probe_basic-dev/docs_src",
+        sphinx_sourcedir="/home/bb/buildbot/worker/probe_basic-dev/sources/docs_src/source/",
+        strict_warnings=False,
+        env={"LANG": "en_EN.UTF-8"},
+        workdir="sources/docs_src/source/"
+    )
+)
+
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
+                                               command=["git", "init"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="add remote repository",
+                                               command=["git", "remote", "add", "origin", "git@github.com:kcjengr/probe_basic.git"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="switch branch",
+                                               command=["git", "checkout", "-b", "gh-pages"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="add docs",
+                                               command=["git", "add", "."],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="commit docs",
+                                               command=["git", "commit", "-m", "Deploy docs"],
+                                               workdir="docs_src/"))
+
+factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="push docs",
+                                               command=["git", "push", "--force", "origin", "gh-pages"],
+                                               workdir="docs_src/"))
