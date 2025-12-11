e3346f5845b9d95b45370fad7b521c166ed45ef8
TurBoss <j.l.toledano.l@gmail.com>
2025-12-11 08:07:32 +0100

    fix path in docs build stop probe_basic

--- probe_basic_factory/x86/dev/probe_basic_pyqt5_x86_dev_factory.py
+++ probe_basic_factory/x86/dev/probe_basic_pyqt5_x86_dev_factory.py
@@ -83,8 +83,8 @@ factory_probe_basic_pyqt5_x86_dev.addStep(
         name="compile sphinx docs",
         haltOnFailure=True,
         sphinx="sphinx-build",
-        sphinx_builddir="/home/bb/work/probe_basic-pyqt5-x86-dev/docs_src",
-        sphinx_sourcedir="/home/bb/work/probe_basic-pyqt5-x86-dev/docs_src/source/",
+        sphinx_builddir="/home/bb/work/probe_basic-pyqt5-x86-dev/sources/docs_src",
+        sphinx_sourcedir="/home/bb/work/probe_basic-pyqt5-x86-dev/sources/docs_src/source/",
         strict_warnings=False,
         env={"LANG": "en_EN.UTF-8"},
         workdir="sources/docs_src/source/"
