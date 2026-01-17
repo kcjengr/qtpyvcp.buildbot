d7f42df257a2145d0bf281ab20905446bcdb7c0a
TurBoss <j.l.toledano.l@gmail.com>
2026-01-17 05:53:05 +0100

    fix typo

--- monokrom_factory/x86/dev/monokrom_pyqt5_x86_dev_factory.py
+++ monokrom_factory/x86/dev/monokrom_pyqt5_x86_dev_factory.py
@@ -71,7 +71,7 @@ factory_monokrom_pyqt5_x86_dev.addStep(steps.ShellCommand(
 # upload files to http server
 factory_monokrom_pyqt5_x86_dev.addStep(steps.FileUpload(
     name="upload files to http server",
-    workersrc=util.Interpolate("/home/bb/work/monokrom-pyqt5-x86-dev/python3-monokorm_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
+    workersrc=util.Interpolate("/home/bb/work/monokrom-pyqt5-x86-dev/python3-monokrom_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
     masterdest=util.Interpolate("/home/buildbot/repo/monokrom-pyqt5-x86-dev/python3-monokrom_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb")))
 
 # upload files to apt server
