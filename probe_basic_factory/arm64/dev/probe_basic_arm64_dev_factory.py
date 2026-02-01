5d650a4dd2679079f6ef35272192b24ce56c29e7
TurBoss <j.l.toledano.l@gmail.com>
2026-02-01 16:04:04 +0100

    fix probe_basic arm64 builder

--- probe_basic_factory/arm64/dev/probe_basic_arm64_dev_factory.py
+++ probe_basic_factory/arm64/dev/probe_basic_arm64_dev_factory.py
@@ -7,67 +7,66 @@
 from buildbot.plugins import steps, util
 from packaging.version import Version, parse
 
-factory_probe_basic_pi4 = util.BuildFactory()
+factory_probe_basic_am64_dev = util.BuildFactory()
 
 
 # download sources
-factory_probe_basic_pi4.addStep(steps.GitHub(name="download sources",
+factory_probe_basic_am64_dev.addStep(steps.GitHub(name="download sources",
                                              repourl='git@github.com:kcjengr/probe_basic.git',
                                              branch='main',
                                              mode='full',
                                              submodules=False,
                                              workdir="sources/"))
-
 # git fetch
-factory_probe_basic_pi4.addStep(steps.ShellCommand(
+factory_probe_basic_am64_dev.addStep(steps.ShellCommand(
     name="git fetch",
     command=["/bin/sh", "-c", "git fetch --all"],
     workdir="sources/"))
-
-# git pull
-factory_probe_basic_pi4.addStep(steps.ShellCommand(
-    name="git pull",
-    command=["/bin/sh", "-c", "git pull origin main"],
-    workdir="sources/"))
-
 # get git tag
-factory_probe_basic_pi4.addStep(steps.SetPropertyFromCommand(
+factory_probe_basic_am64_dev.addStep(steps.SetPropertyFromCommand(
     name="get git tag",
     command=["git", "describe", "--abbrev=0", "--tags"],
     property="tag",
     workdir="sources/"))
 
-# compile resources
-factory_probe_basic_pi4.addStep(steps.ShellCommand(
-    name="compile resources",
-    command=["qcompile", "."],
+# get git commit count since last tag
+factory_probe_basic_am64_dev.addStep(steps.SetPropertyFromCommand(
+    name="get git commit count since last tag",
+    command=["git", "rev-list", "--count", "--branches", util.Interpolate("^refs/tags/%(prop:tag)s")],
+    property="minor_version",
     workdir="sources/"))
 
+# store version file
+factory_probe_basic_am64_dev.addStep(steps.ShellCommand(
+    name="store version file",
+    command=["/bin/sh", "-c", util.Interpolate('echo %(prop:tag)s-%(prop:minor_version)s > pb_dev_version.txt')],
+    workdir="/home/buildbot/versions/"))
+
 # create changelog
-factory_probe_basic_pi4.addStep(steps.ShellCommand(
+factory_probe_basic_am64_dev.addStep(steps.ShellCommand(
     name="create changelog",
     env={'EMAIL': "lcvette1@gmail.com"},
-    command=["dch", "--create", "--distribution", "stable", "--package", "probe-basic", "--newversion", util.Interpolate("%(prop:tag)s"), "Stable version."],
+    command=["dch", "--create", "--distribution", "unstable", "--package", "probe-basic", "--newversion", util.Interpolate("%(prop:tag)s-%(prop:minor_version)s.dev"), "Unstable Release version."],
     workdir="sources/"))
 
 # build debs
-factory_probe_basic_pi4.addStep(steps.ShellCommand(
+factory_probe_basic_am64_dev.addStep(steps.ShellCommand(
     name="build debs",
     env={'DEB_BUILD_OPTIONS': "nocheck"},
     command=["dpkg-buildpackage", "-b", "-uc"],
     workdir="sources/"))
 
 # copy files to the http repo
-factory_probe_basic_pi4.addStep(steps.FileUpload(
+factory_probe_basic_am64_dev.addStep(steps.FileUpload(
     name="copy files to the http repo",
-    workersrc=util.Interpolate("/home/buildbot/workdir/probe_basic-pi4/python3-probe-basic_%(prop:tag)s_arm64.deb"),
-     masterdest=util.Interpolate("/home/buildbot/repo/probe-basic-pi4/python3-probe-basic_%(prop:tag)s_arm64.deb")
+    workersrc=util.Interpolate("/home/buildbot/workdir/probe_basic-pi4-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"),
+     masterdest=util.Interpolate("/home/buildbot/repo/probe-basic-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb")
     )
 )
 
 
 # delete old files from apt directory
-# factory_probe_basic_pi4.addStep(steps.ShellCommand(
+# factory_probe_basic_pi4_dev.addStep(steps.ShellCommand(
 #     name="delete files from apt directory",
 #     command=["sh",
 #              "/home/buildbot/buildbot/master/scripts/clean_apt_develop.sh",
@@ -76,48 +75,48 @@ factory_probe_basic_pi4.addStep(steps.FileUpload(
 #     workdir="sources/"))
 
 # move new files to the apt repo
-factory_probe_basic_pi4.addStep(steps.FileUpload(
+factory_probe_basic_am64_dev.addStep(steps.FileUpload(
     name="move new files to the apt repo",
-    workersrc=util.Interpolate("/home/buildbot/workdir/probe_basic-pi4/python3-probe-basic_%(prop:tag)s_arm64.deb"),
-    masterdest=util.Interpolate("/home/buildbot/debian/apt/pool/main/stable/python3-probe-basic_%(prop:tag)s_arm64.deb")
+    workersrc=util.Interpolate("/home/buildbot/workdir/probe_basic-pi4-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"),
+    masterdest=util.Interpolate("/home/buildbot/debian/apt/pool/main/develop/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb")
     )
 )
 
 # delete files from build directory
-# factory_probe_basic_pi4.addStep(steps.ShellCommand(
+# factory_probe_basic_pi4_dev.addStep(steps.ShellCommand(
 #     name="delete files from build directory",
 #     command=["rm", util.Interpolate("/home/buildbot/buildbot/worker/monokrom-dev/python3-monokrom_%(prop:tag)s-%(prop:minor_version)s.dev_all.deb")],
 #     workdir="sources/"))
 
 # scan new packages in apt repository
-factory_probe_basic_pi4.addStep(steps.MasterShellCommand(
+factory_probe_basic_am64_dev.addStep(steps.MasterShellCommand(
     name="scan new packages in apt repository",
-    command="/home/buildbot/buildbot/master/scripts/do_apt_stable.sh"
+    command="/home/buildbot/buildbot/master/scripts/do_apt_develop.sh"
     )
 )
 
 
 
-# factory_probe_basic_pi4.addStep(steps.GitHub(name="downlaod static docs",
+# factory_probe_basic_pi4_dev.addStep(steps.GitHub(name="downlaod static docs",
 #                                              repourl='git@github.com:kcjengr/probe_basic.git',
 #                                              origin="origin",
 #                                              branch="gh-pages",
 #                                              mode='full',
 #                                              workdir="docs/"))
 #
-# factory_probe_basic_pi4.addStep(steps.ShellCommand(name="reset gh-pages",
+# factory_probe_basic_pi4_dev.addStep(steps.ShellCommand(name="reset gh-pages",
 #                                                    command=["git", "symbolic-ref", "HEAD", "refs/heads/gh-pages"],
 #                                                    workdir="docs/"))
 #
-# factory_probe_basic_pi4.addStep(steps.ShellCommand(name="delete git index",
+# factory_probe_basic_pi4_dev.addStep(steps.ShellCommand(name="delete git index",
 #                                                    command=["rm", ".git/index"],
 #                                                    workdir="docs/"))
 #
-# factory_probe_basic_pi4.addStep(steps.ShellCommand(name="clean gh-pages",
+# factory_probe_basic_pi4_dev.addStep(steps.ShellCommand(name="clean gh-pages",
 #                                                    command=["git", "clean", "-fdx"],
 #                                                    workdir="docs/"))
 #
-# factory_probe_basic_pi4.addStep(
+# factory_probe_basic_pi4_dev.addStep(
 #     steps.Sphinx(
 #         name="compile sphinx docs",
 #         haltOnFailure=True,
