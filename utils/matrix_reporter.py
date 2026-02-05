9d891ba5cefdb6c0f3dd375d8dfb3a38ea274336
Chris Polanski <small.shop.concepts@gmail.com>
2026-02-05 14:28:41 -0500

    Enable and fix MatrixReporter for build notifications
    - Fixed MatrixReporter code issues:
      * Removed syntax error in _create_default_generators (trailing comma)
      * Properly integrated asyncio with Twisted using threads.deferToThread
      * Simplified message formatting with MessageFormatterRenderable
      * Added proper error handling and logging
      * Marked userToken as secret
    
    - Added Matrix configuration to bot_config.py:
      * matrix_server, matrix_user, matrix_user_token
      * matrix_room_id, matrix_debug settings
    
    - Enabled MatrixReporter in master.cfg
    - Installed matrix-nio library for Matrix protocol support

--- utils/matrix_reporter.py
+++ utils/matrix_reporter.py
@@ -1,49 +1,33 @@
 import asyncio
 import logging
 
-from datetime import datetime
-from nio import AsyncClient, MatrixRoom, RoomMessageText
+from nio import AsyncClient
 
-from twisted.internet import defer
+from twisted.internet import defer, threads
 from buildbot.reporters.generators.build import BuildStatusGenerator
-from buildbot.reporters.message import MessageFormatterFunction, MessageFormatter
+from buildbot.reporters.message import MessageFormatterRenderable
 from buildbot.reporters.base import ReporterBase
 from buildbot.reporters.utils import merge_reports_prop, merge_reports_prop_take_first
 
-# Enable debug logging for the nio client
-logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
 class MatrixReporter(ReporterBase):
     name = "MatrixReporter"
-    secrets = []
+    secrets = ['userToken']
 
-    server_url = ""
-    user_name = ""
-    user_pass = ""
-    room_id = ""
-    debug = False
-
-    def checkConfig(self, serverUrl, userName=None, userToken=None, roomID=None, headers=None,
-                        debug=None, verify=None, generators=None, **kwargs):
+    def checkConfig(self, serverUrl, userName=None, userToken=None, roomID=None,
+                    debug=False, generators=None, **kwargs):
 
         if generators is None:
             generators = self._create_default_generators()
 
-        self.server_url = serverUrl
-        self.user_name = userName
-        self.user_token = userToken
-        self.room_id = roomID
-        self.debug = debug
-
         super().checkConfig(generators=generators, **kwargs)
 
     @defer.inlineCallbacks
-    def reconfigService(self, serverUrl, userName=None, userToken=None, roomID=None, headers=None,
-                        debug=None, verify=None, generators=None, **kwargs):
-        self.debug = debug
-        self.verify = verify
-
+    def reconfigService(self, serverUrl, userName=None, userToken=None, roomID=None,
+                        debug=False, generators=None, **kwargs):
+        
         self.server_url = serverUrl
         self.user_name = userName
         self.user_token = userToken
@@ -55,67 +39,67 @@ class MatrixReporter(ReporterBase):
 
         yield super().reconfigService(generators=generators, **kwargs)
 
+        # Initialize Matrix client
         self._client = AsyncClient(self.server_url, self.user_name)
-
         self._client.access_token = self.user_token
         self._client.user_id = self.user_name
         self._client.device_id = "buildbot"
 
-        # yield self._client.login()
+        if self.debug:
+            log.info(f"MatrixReporter configured for {self.server_url}, room: {self.room_id}")
 
     def _create_default_generators(self):
-
-        BuildStatusGenerator(
-                add_patch=True, message_formatter=MessageFormatter(template_type='html')
-        ),
-        formatter = MessageFormatterFunction(lambda context: context['build'], 'plain')
-        return [BuildStatusGenerator(message_formatter=formatter, report_new=True)]
+        """Create default message generator for build status updates"""
+        formatter = MessageFormatterRenderable("Build {{ build['builder']['name'] }} #{{ build['number'] }}: {{ build['state_string'] }}")
+        return [BuildStatusGenerator(message_formatter=formatter)]
 
     @defer.inlineCallbacks
     def sendMessage(self, reports):
+        """Send build report to Matrix room"""
         body = merge_reports_prop(reports, 'body')
         subject = merge_reports_prop_take_first(reports, 'subject')
-        type = merge_reports_prop_take_first(reports, 'type')
-        results = merge_reports_prop(reports, 'results')
+        build_results = merge_reports_prop_take_first(reports, 'results')
         builds = merge_reports_prop(reports, 'builds')
-        users = merge_reports_prop(reports, 'users')
-        patches = merge_reports_prop(reports, 'patches')
-        logs = merge_reports_prop(reports, 'logs')
-        worker = merge_reports_prop_take_first(reports, 'worker')
 
-        print("#########################")
-        print("BODY @@@@@@@@@@@@@@@")
-        print(body)
-        print("SUBJECT @@@@@@@@@@@@@@@")
-        print(subject)
-        print("TYPE @@@@@@@@@@@@@@@")
-        print(type)
-        print("RESULTS @@@@@@@@@@@@@@@")
-        print(results)
-        print("BUILD @@@@@@@@@@@@@@@")
-        print(builds)
-        print("USERS @@@@@@@@@@@@@@@")
-        print(users)
-        print("PATCHES @@@@@@@@@@@@@@@")
-        print(patches)
-        print("LOGS @@@@@@@@@@@@@@@")
-        print(logs)
-        print("WORKER @@@@@@@@@@@@@@@")
-        print(worker)
-        print("#########################")
+        # Format message
+        if body:
+            message = body
+        elif subject:
+            message = subject
+        else:
+            message = "Build status update"
 
-        event_loop = asyncio.get_event_loop()
+        if self.debug:
+            log.debug("=" * 50)
+            log.debug(f"Subject: {subject}")
+            log.debug(f"Body: {body}")
+            log.debug(f"Results: {build_results}")
+            log.debug(f"Builds: {builds}")
+            log.debug("=" * 50)
 
-        yield self.send(subject)
+        # Send to Matrix using thread pool to avoid blocking Twisted reactor
+        try:
+            yield threads.deferToThread(self._send_to_matrix, message)
+        except Exception as e:
+            log.error(f"Failed to send Matrix message: {e}")
 
-    def send(self, subject, **kwargs):
-        event_loop = asyncio.get_event_loop()
-
-        asyncio.ensure_future(
-            self._client.room_send(
-                room_id=self.room_id,
-                message_type="m.room.message",
-                content={"msgtype": "m.text", "body": f"{subject}"}
-            ),
-            loop=event_loop
-        )
+    def _send_to_matrix(self, message):
+        """Send message to Matrix room (runs in thread pool)"""
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        
+        try:
+            result = loop.run_until_complete(
+                self._client.room_send(
+                    room_id=self.room_id,
+                    message_type="m.room.message",
+                    content={"msgtype": "m.text", "body": str(message)}
+                )
+            )
+            
+            if self.debug:
+                log.debug(f"Matrix send result: {result}")
+                
+            return result
+        finally:
+            loop.close()
