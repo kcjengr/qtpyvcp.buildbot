b5ac9d81c4b9464bd4f520809dc99a5b6340f1be
Chris Polanski <small.shop.concepts@gmail.com>
2026-02-05 14:34:13 -0500

    Revert bot_config.py changes - keep Matrix config in pass_file instead

--- bot_config.py
+++ bot_config.py
@@ -2,15 +2,7 @@ github_phrase = ''
 web_password = ''
 worker = ''
 
-# Matrix notification settings
-matrix_server = 'https://matrix.org'  # Your Matrix homeserver URL
-matrix_user = '@buildbot:matrix.org'  # Your bot's Matrix user ID
-matrix_user_token = ''  # Bot's access token (REQUIRED - set this!)
-matrix_room_id = '!yourRoomId:matrix.org'  # Room ID to send notifications to
-matrix_debug = False  # Set to True for verbose Matrix logging
-
-# Other tokens
-matrix_access_token = ''  # DEPRECATED - use matrix_user_token instead
+matrix_access_token = ''
 github_kcjengr_token = ''
 pypi_qtpyvcp_token = ''
 pypi_probe_basic_token = ''
