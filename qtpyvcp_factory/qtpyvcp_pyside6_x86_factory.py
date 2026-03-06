916174a6d3038a72116c8a01ab256b853b8e3c23
TurBoss <j.l.toledano.l@gmail.com>
2026-03-07 00:45:41 +0100

    disable file version qtpyvcp pyside6 amd64

--- qtpyvcp_factory/qtpyvcp_pyside6_x86_factory.py
+++ qtpyvcp_factory/qtpyvcp_pyside6_x86_factory.py
@@ -10,36 +10,52 @@ from buildbot.plugins import steps, util
 factory_qtpyvcp_pyside6_x86 = util.BuildFactory()
 
 # download sources
-factory_qtpyvcp_pyside6_x86.addStep(steps.GitHub(name="download sources",
-                                                 repourl='git@github.com:kcjengr/qtpyvcp.git',
-                                                 branch='main',
-                                                 mode='full',
-                                                 submodules=False,
-                                                 workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.GitHub(
+        name="download sources",
+        repourl="git@github.com:kcjengr/qtpyvcp.git",
+        branch="main",
+        mode="full",
+        submodules=False,
+        workdir="sources/",
+    )
+)
 # git fetch
-factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
-    name="git fetch",
-    command=["/bin/sh", "-c", "git fetch --all"],
-    workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.ShellCommand(
+        name="git fetch",
+        command=["/bin/sh", "-c", "git fetch --all"],
+        workdir="sources/",
+    )
+)
 
 # git pull
-factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
-    name="git pull",
-    command=["/bin/sh", "-c", "git pull origin main"],
-    workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.ShellCommand(
+        name="git pull",
+        command=["/bin/sh", "-c", "git pull origin main"],
+        workdir="sources/",
+    )
+)
 
 # get git tag
-factory_qtpyvcp_pyside6_x86.addStep(steps.SetPropertyFromCommand(
-    name="get git tag",
-    command=["git", "describe", "--abbrev=0", "--tags"],
-    property="tag",
-    workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.SetPropertyFromCommand(
+        name="get git tag",
+        command=["git", "describe", "--abbrev=0", "--tags"],
+        property="tag",
+        workdir="sources/",
+    )
+)
 
 # checkout the tag
-factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
-    name="checkout tag",
-    command=["git", "checkout", util.Interpolate("%(prop:tag)s")],
-    workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.ShellCommand(
+        name="checkout tag",
+        command=["git", "checkout", util.Interpolate("%(prop:tag)s")],
+        workdir="sources/",
+    )
+)
 
 # not needed by release
 # # get git commit count since last tag
@@ -50,31 +66,61 @@ factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
 #     workdir="sources/"))
 
 # store version file
-factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
-    name="store version file",
-    command=["/bin/sh", "-c", util.Interpolate('echo %(prop:tag)s > qtpyvcp_version.txt')],
-    workdir="/home/buildbot/versions/"))
+# factory_qtpyvcp_pyside6_x86.addStep(
+#     steps.ShellCommand(
+#         name="store version file",
+#         command=[
+#             "/bin/sh",
+#             "-c",
+#             util.Interpolate("echo %(prop:tag)s > qtpyvcp_version.txt"),
+#         ],
+#         workdir="/home/buildbot/versions/",
+#     )
+# )
 
 # create changelog
-factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
-    name="create changelog",
-    env={'EMAIL': "j.l.toledano.l@gmail.com"},
-    command=["dch", "--create", "--distribution", "trixie", "--package", "qtpyvcp", "--newversion", util.Interpolate("%(prop:tag)s"), "Trixie version."],
-    workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.ShellCommand(
+        name="create changelog",
+        env={"EMAIL": "j.l.toledano.l@gmail.com"},
+        command=[
+            "dch",
+            "--create",
+            "--distribution",
+            "trixie",
+            "--package",
+            "qtpyvcp",
+            "--newversion",
+            util.Interpolate("%(prop:tag)s"),
+            "Trixie version.",
+        ],
+        workdir="sources/",
+    )
+)
 
 # build debs
-factory_qtpyvcp_pyside6_x86.addStep(steps.ShellCommand(
-    name="build debs",
-    env={'DEB_BUILD_OPTIONS': "nocheck"},
-    command=["dpkg-buildpackage", "-b", "-uc"],
-    workdir="sources/"))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.ShellCommand(
+        name="build debs",
+        env={"DEB_BUILD_OPTIONS": "nocheck"},
+        command=["dpkg-buildpackage", "-b", "-uc"],
+        workdir="sources/",
+    )
+)
 
 # upload files to http server
-factory_qtpyvcp_pyside6_x86.addStep(steps.FileUpload(
-    name="upload files to http server",
-    workersrc=util.Interpolate("/home/bb/work/qtpyvcp-pyside6-x86/python3-qtpyvcp_%(prop:tag)s_amd64.deb"),
-    masterdest=util.Interpolate("/home/buildbot/repo/qtpyvcp-pyside6-x86/python3-qtpyvcp_%(prop:tag)s_amd64.deb"),
-    mode=0o644))
+factory_qtpyvcp_pyside6_x86.addStep(
+    steps.FileUpload(
+        name="upload files to http server",
+        workersrc=util.Interpolate(
+            "/home/bb/work/qtpyvcp-pyside6-x86/python3-qtpyvcp_%(prop:tag)s_amd64.deb"
+        ),
+        masterdest=util.Interpolate(
+            "/home/buildbot/repo/qtpyvcp-pyside6-x86/python3-qtpyvcp_%(prop:tag)s_amd64.deb"
+        ),
+        mode=0o644,
+    )
+)
 
 # # disabled needs apt structure things
 # # copy new files to the apt repo
