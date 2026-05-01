22f82e2331981065f3b45589a716fe2a7cfbd7c5
Chris Polanski <small.shop.concepts@gmail.com>
2026-05-01 13:51:23 -0400

    move probe basic docs building responsibility to pyside6 factory

--- probe_basic_factory/probe_basic_pyqt5_x86_dev_factory.py
+++ probe_basic_factory/probe_basic_pyqt5_x86_dev_factory.py
@@ -9,114 +9,89 @@ from packaging.version import Version, parse
 
 factory_probe_basic_pyqt5_x86_dev = util.BuildFactory()
 
-
 # download sources
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.GitHub(name="download sources",
-                                             repourl='git@github.com:kcjengr/probe_basic.git',
-                                             branch='main',
-                                             mode='full',
-                                             submodules=False,
-                                             workdir="sources/"))
+    repourl='git@github.com:kcjengr/probe_basic.git',
+    branch='main',
+    mode='full',
+    submodules=False,
+    workdir="sources/",
+    )
+)
 
 # git fetch
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
     name="git fetch",
     command=["/bin/sh", "-c", "git fetch --all"],
-    workdir="sources/"))
+    workdir="sources/",
+    )
+)
 
 # get git tag
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.SetPropertyFromCommand(
     name="get git tag",
     command=["git", "describe", "--abbrev=0", "--tags"],
     property="tag",
-    workdir="sources/"))
+    workdir="sources/",
+    )
+)
 
 # get git commit count since last tag
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.SetPropertyFromCommand(
     name="get git commit count since last tag",
     command=["git", "rev-list", "--count", "--branches", util.Interpolate("^refs/tags/%(prop:tag)s")],
     property="minor_version",
-    workdir="sources/"))
+    workdir="sources/",
+    )
+)
 
 # store version file
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
     name="store version file",
     command=["/bin/sh", "-c", util.Interpolate('echo %(prop:tag)s-%(prop:minor_version)s > pb_dev_version.txt')],
-    workdir="/home/bb/versions/"))
+    workdir="/home/bb/versions/",
+    )
+)
 
 # create changelog
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
     name="create changelog",
     env={'EMAIL': "lcvette1@gmail.com"},
     command=["dch", "--create", "--distribution", "unstable", "--package", "probe-basic", "--newversion", util.Interpolate("%(prop:tag)s-%(prop:minor_version)s.dev"), "Unstable Release version."],
-    workdir="sources/"))
+    workdir="sources/",
+    )
+)
 
 # build debs
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(
     name="build debs",
     env={'DEB_BUILD_OPTIONS': "nocheck"},
     command=["dpkg-buildpackage", "-b", "-uc"],
-    workdir="sources/"))
-
+    workdir="sources/",
+    )
+)
 
 # upload files to http server
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.FileUpload(
     name="upload files to http server",
     workersrc=util.Interpolate("/home/bb/work/probe_basic-pyqt5-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
     masterdest=util.Interpolate("/home/buildbot/repo/probe_basic-pyqt5-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
-    mode=0o644))
+    mode=0o644,
+    )
+)
 
 # upload files to apt server
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.FileUpload(
     name="upload files to apt server",
     workersrc=util.Interpolate("/home/bb/work/probe_basic-pyqt5-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
-    masterdest=util.Interpolate("/home/buildbot/debian/apt/pool/main/bookworm-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb")))
-
+    masterdest=util.Interpolate("/home/buildbot/debian/apt/pool/main/bookworm-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
+    )
+)
 
 # scan new packages in apt repository
 factory_probe_basic_pyqt5_x86_dev.addStep(steps.MasterShellCommand(
     name="scan new packages in apt repository",
     command=["sh", "/home/buildbot/buildbot/master/scripts/do_apt_bookworm_dev.sh"],
-    workdir="/home/buildbot/debian/apt"))
-
-
-
-# delete docs directory
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.RemoveDirectory(name="delete docs directory", dir="docs_src/"))
-
-factory_probe_basic_pyqt5_x86_dev.addStep(
-    steps.Sphinx(
-        name="compile sphinx docs",
-        haltOnFailure=True,
-        sphinx="/home/bb/.venv/bin/sphinx-build",
-        sphinx_builddir="/home/bb/work/probe_basic-pyqt5-x86-dev/docs_src",
-        sphinx_sourcedir="/home/bb/work/probe_basic-pyqt5-x86-dev/sources/docs_src/source",
-        strict_warnings=False,
-        env={"LANG": "en_US.UTF-8"},
-        workdir="/home/bb/work/probe_basic-pyqt5-x86-dev/sources/docs_src/source"
+    workdir="/home/buildbot/debian/apt",
     )
 )
-
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
-                                               command=["bash", "-c", "git init"],
-                                               workdir="docs_src/"))
-
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="add remote repository",
-                                               command=["bash", "-c", "git remote add origin git@github.com:kcjengr/probe_basic.git"],
-                                               workdir="docs_src/"))
-
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="switch branch",
-                                               command=["bash", "-c", "git checkout -b gh-pages"],
-                                               workdir="docs_src/"))
-
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="add docs",
-                                               command=["bash", "-c", "git add ."],
-                                               workdir="docs_src/"))
-
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="commit docs",
-                                               command=["bash", "-c", "git commit -m \"Deploy docs\""],
-                                               workdir="docs_src/"))
-
-factory_probe_basic_pyqt5_x86_dev.addStep(steps.ShellCommand(name="push docs",
-                                               command=["bash", "-c", "git push --force origin gh-pages"],
-                                               workdir="docs_src/"))
--- probe_basic_factory/probe_basic_pyside6_x86_dev_factory.py
+++ probe_basic_factory/probe_basic_pyside6_x86_dev_factory.py
@@ -10,147 +10,182 @@ from packaging.version import Version, parse
 factory_probe_basic_pyside6_x86_dev = util.BuildFactory()
 
 # download sources
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.GitHub(
-        name="download sources",
-        repourl="git@github.com:kcjengr/probe_basic.git",
-        branch="pyside6",
-        mode="full",
-        submodules=False,
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.GitHub(
+    name="download sources",
+    repourl="git@github.com:kcjengr/probe_basic.git",
+    branch="pyside6",
+    mode="full",
+    submodules=False,
+    workdir="sources/",
     )
 )
 
 # git fetch
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.ShellCommand(
-        name="git fetch",
-        command=["/bin/sh", "-c", "git fetch --all"],
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(
+    name="git fetch",
+    command=["/bin/sh", "-c", "git fetch --all"],
+    workdir="sources/",
     )
 )
 
 # git pull
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.ShellCommand(
-        name="git pull",
-        command=["/bin/sh", "-c", "git pull origin pyside6"],
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(
+    name="git pull",
+    command=["/bin/sh", "-c", "git pull origin pyside6"],
+    workdir="sources/",
     )
 )
 
 # update venv
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.ShellCommand(
-        name="update venv",
-        command=["/home/bb/.venv_dev/bin/python", "-m", "pip", "install", "-U", "-e", "."],
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(
+    name="update venv",
+    command=["/home/bb/.venv_dev/bin/python", "-m", "pip", "install", "-U", "-e", "."],
+    workdir="sources/",
     )
 )
 
 # get git tag
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.SetPropertyFromCommand(
-        name="get git tag",
-        command=["git", "describe", "--abbrev=0", "--tags"],
-        property="tag",
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.SetPropertyFromCommand(
+    name="get git tag",
+    command=["git", "describe", "--abbrev=0", "--tags"],
+    property="tag",
+    workdir="sources/",
     )
 )
 
 # get git commit count since last tag
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.SetPropertyFromCommand(
-        name="get git commit count since last tag",
-        command=[
-            "git",
-            "rev-list",
-            "--count",
-            "--branches",
-            util.Interpolate("^refs/tags/%(prop:tag)s"),
-        ],
-        property="minor_version",
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.SetPropertyFromCommand(
+    name="get git commit count since last tag",
+    command=[
+        "git",
+        "rev-list",
+        "--count",
+        "--branches",
+        util.Interpolate("^refs/tags/%(prop:tag)s"),
+    ],
+    property="minor_version",
+    workdir="sources/",
     )
 )
 
 # store version file
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.ShellCommand(
-        name="store version file",
-        command=[
-            "/bin/sh",
-            "-c",
-            util.Interpolate(
-                "echo %(prop:tag)s-%(prop:minor_version)s > pb_dev_version.txt"
-            ),
-        ],
-        workdir="/home/bb/versions/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(
+    name="store version file",
+    command=[
+        "/bin/sh",
+        "-c",
+        util.Interpolate(
+            "echo %(prop:tag)s-%(prop:minor_version)s > pb_dev_version.txt"
+        ),
+    ],
+    workdir="/home/bb/versions/",
     )
 )
 
 # create changelog
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.ShellCommand(
-        name="create changelog",
-        env={"EMAIL": "lcvette1@gmail.com"},
-        command=[
-            "dch",
-            "--create",
-            "--distribution",
-            "unstable",
-            "--package",
-            "probe-basic",
-            "--newversion",
-            util.Interpolate("%(prop:tag)s-%(prop:minor_version)s.dev"),
-            "Unstable Release version.",
-        ],
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(
+    name="create changelog",
+    env={"EMAIL": "lcvette1@gmail.com"},
+    command=[
+        "dch",
+        "--create",
+        "--distribution",
+        "unstable",
+        "--package",
+        "probe-basic",
+        "--newversion",
+        util.Interpolate("%(prop:tag)s-%(prop:minor_version)s.dev"),
+        "Unstable Release version.",
+    ],
+    workdir="sources/",
     )
 )
 
 # build debs
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.ShellCommand(
-        name="build debs",
-        env={"DEB_BUILD_OPTIONS": "nocheck"},
-        command=["dpkg-buildpackage", "-b", "-uc"],
-        workdir="sources/",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(
+    name="build debs",
+    env={"DEB_BUILD_OPTIONS": "nocheck"},
+    command=["dpkg-buildpackage", "-b", "-uc"],
+    workdir="sources/",
     )
 )
 
 # upload files to http server
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.FileUpload(
-        name="upload files to http server",
-        workersrc=util.Interpolate(
-            "/home/bb/work/probe_basic-pyside6-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"
-        ),
-        masterdest=util.Interpolate(
-            "/home/buildbot/repo/probe_basic-pyside6-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"
-        ),
-        mode=0o644,
+factory_probe_basic_pyside6_x86_dev.addStep(steps.FileUpload(
+    name="upload files to http server",
+    workersrc=util.Interpolate("/home/bb/work/probe_basic-pyside6-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
+    masterdest=util.Interpolate("/home/buildbot/repo/probe_basic-pyside6-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
+    mode=0o644,
     )
 )
 
 # upload files to apt server
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.FileUpload(
-        name="upload files to apt server",
-        workersrc=util.Interpolate(
-            "/home/bb/work/probe_basic-pyside6-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"
-        ),
-        masterdest=util.Interpolate(
-            "/home/buildbot/debian/apt/pool/main/trixie-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"
-        ),
+factory_probe_basic_pyside6_x86_dev.addStep(steps.FileUpload(
+    name="upload files to apt server",
+    workersrc=util.Interpolate("/home/bb/work/probe_basic-pyside6-x86-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
+    masterdest=util.Interpolate("/home/buildbot/debian/apt/pool/main/trixie-dev/python3-probe-basic_%(prop:tag)s-%(prop:minor_version)s.dev_amd64.deb"),
     )
 )
 
 # scan new packages in apt repository
-factory_probe_basic_pyside6_x86_dev.addStep(
-    steps.MasterShellCommand(
-        name="scan new packages in apt repository",
-        command=["sh", "/home/buildbot/buildbot/master/scripts/do_apt_trixie_dev.sh"],
-        workdir="/home/buildbot/debian/apt",
+factory_probe_basic_pyside6_x86_dev.addStep(steps.MasterShellCommand(
+    name="scan new packages in apt repository",
+    command=["sh", "/home/buildbot/buildbot/master/scripts/do_apt_trixie_dev.sh"],
+    workdir="/home/buildbot/debian/apt",
+    )
+)
+
+# delete docs directory
+factory_probe_basic_pyside6_x86_dev.addStep(steps.RemoveDirectory(
+    name="delete docs directory",
+    dir="docs_src/",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.Sphinx(
+    name="compile sphinx docs",
+    haltOnFailure=True,
+    sphinx="/home/bb/.venv/bin/sphinx-build",
+    sphinx_builddir="/home/bb/work/probe_basic-pyside6-x86-dev/docs_src",
+    sphinx_sourcedir="/home/bb/work/probe_basic-pyside6-x86-dev/sources/docs_src/source",
+    strict_warnings=False,
+    env={"LANG": "en_US.UTF-8"},
+    workdir="/home/bb/work/probe_basic-pyside6-x86-dev/sources/docs_src/source",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(name="Initialize docs repository",
+    command=["bash", "-c", "git init"],
+    workdir="docs_src/",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(name="add remote repository",
+    command=["bash", "-c", "git remote add origin git@github.com:kcjengr/probe_basic.git"],
+    workdir="docs_src/",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(name="switch branch",
+    command=["bash", "-c", "git checkout -b gh-pages"],
+    workdir="docs_src/",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(name="add docs",
+    command=["bash", "-c", "git add ."],
+    workdir="docs_src/",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(name="commit docs",
+    command=["bash", "-c", "git commit -m \"Deploy docs\""],
+    workdir="docs_src/",
+    )
+)
+
+factory_probe_basic_pyside6_x86_dev.addStep(steps.ShellCommand(name="push docs",
+    command=["bash", "-c", "git push --force origin gh-pages"],
+    workdir="docs_src/",
     )
 )
