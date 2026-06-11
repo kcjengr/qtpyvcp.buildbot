24568081a68b92a1895dce94bce7df7a4c7575ea
TurBoss <j.l.toledano.l@gmail.com>
2026-06-11 02:25:08 +0200

    fix missing dev arm pyqt5 worker steps

--- qtpyvcp_factory/qtpyvcp_pyqt5_arm64_dev_factory.py
+++ qtpyvcp_factory/qtpyvcp_pyqt5_arm64_dev_factory.py
@@ -39,6 +39,23 @@ factory_qtpyvcp_pyqt5_arm64_dev.addStep(
     )
 )
 
+# update venv
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.ShellCommand(
+        name="update venv",
+        command=[
+            "/home/bb/.venv_dev/bin/python",
+            "-m",
+            "pip",
+            "install",
+            "-U",
+            "-e",
+            ".",
+        ],
+        workdir="sources/",
+    )
+)
+
 # get git tag
 factory_qtpyvcp_pyqt5_arm64_dev.addStep(
     steps.SetPropertyFromCommand(
@@ -65,11 +82,30 @@ factory_qtpyvcp_pyqt5_arm64_dev.addStep(
     )
 )
 
-# # store version file
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.ShellCommand(
-#     name="store version file",
-#     command=["/bin/sh", "-c", util.Interpolate('echo %(prop:tag)s-%(prop:minor_version)s > qtpyvcp_dev_version.txt')],
-#     workdir="/home/buildbot/versions/"))
+# store version file
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.ShellCommand(
+        name="store version file",
+        command=[
+            "/bin/sh",
+            "-c",
+            util.Interpolate(
+                "echo %(prop:tag)s-%(prop:minor_version)s > qtpyvcp_dev_version.txt"
+            ),
+        ],
+        workdir="/home/bb/versions/",
+    )
+)
+
+# delete previous changelog
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.ShellCommand(
+        name="Delete previous changelog",
+        env={},
+        command=["rm", "-rf", "debian/changelog"],
+        workdir="sources/",
+    )
+)
 
 # create changelog
 factory_qtpyvcp_pyqt5_arm64_dev.addStep(
@@ -94,6 +130,20 @@ factory_qtpyvcp_pyqt5_arm64_dev.addStep(
 # build debs
 factory_qtpyvcp_pyqt5_arm64_dev.addStep(
     steps.ShellCommand(
+        name="clean stale native artifacts",
+        command=[
+            "/bin/sh",
+            "-c",
+            "find src/qtpyvcp/native -type f \\( "
+            "-name '*x86_64-linux-gnu.so' -o -name '*amd64*.so' \\) -delete",
+        ],
+        workdir="sources/",
+    )
+)
+
+# build debs
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.ShellCommand(
         name="build debs",
         env={"DEB_BUILD_OPTIONS": "nocheck"},
         command=["dpkg-buildpackage", "-b", "-uc"],
@@ -101,50 +151,38 @@ factory_qtpyvcp_pyqt5_arm64_dev.addStep(
     )
 )
 
-# # copy files to the http repo on .13 machine
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.FileUpload(
-#     name="copy files to the http repo",
-#     workersrc=util.Interpolate("/home/buildbot/workdir/qtpyvcp-pi4-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"),
-#     masterdest=util.Interpolate("/home/buildbot/repo/qtpyvcp-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"),
-#     mode=0o644))
-#
-#
-# # copy new files to the apt repo on .13 machine
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.FileUpload(
-#     name="copy files to repo",
-#     workersrc=util.Interpolate("/home/buildbot/workdir/qtpyvcp-pi4-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"),
-#     masterdest=util.Interpolate("/home/buildbot/debian/apt/pool/main/develop/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb")
-#     )
-# )
-#
-#
-# # clean up the workdir of old deb files after copying
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.ShellCommand(
-#     name="remove deb files from workdir",
-#     command=["rm", util.Interpolate("python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb")],
-#     workdir="./"
-#     )
-# )
-#
-#
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.ShellCommand(
-#     name="remove changes files from workdir",
-#     command=["rm", util.Interpolate("qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.changes")],
-#     workdir="./"
-#     )
-# )
-#
-#
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.ShellCommand(
-#     name="rmove buildinfo files from workdir",
-#     command=["rm", util.Interpolate("qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.buildinfo")],
-#     workdir="./"
-#     )
-# )
-#
-# #scan new packages in apt repository
-# factory_qtpyvcp_pyqt5_arm64_dev.addStep(steps.MasterShellCommand(
-#     name="scan new packages in apt repository",
-#     command="/home/buildbot/buildbot/master/scripts/do_apt_develop.sh"
-#     )
-# )
+# upload files to http server
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.FileUpload(
+        name="upload files to http server",
+        workersrc=util.Interpolate(
+            "/home/bb/work/qtpyvcp-pyqt5-arm64-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"
+        ),
+        masterdest=util.Interpolate(
+            "/home/buildbot/repo/qtpyvcp-pyqt5-arm64-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"
+        ),
+        mode=0o644,
+    )
+)
+
+# upload files to apt server
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.FileUpload(
+        name="upload files to apt server",
+        workersrc=util.Interpolate(
+            "/home/bb/work/qtpyvcp-pyqt5-arm64-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"
+        ),
+        masterdest=util.Interpolate(
+            "/home/buildbot/debian/apt/pool/main/bookworm-dev/python3-qtpyvcp_%(prop:tag)s-%(prop:minor_version)s.dev_arm64.deb"
+        ),
+    )
+)
+
+# scan new packages in apt repository
+factory_qtpyvcp_pyqt5_arm64_dev.addStep(
+    steps.MasterShellCommand(
+        name="scan new packages in apt repository",
+        command=["sh", "/home/buildbot/buildbot/master/scripts/do_apt_bookworm_dev.sh"],
+        workdir="/home/buildbot/debian/apt",
+    )
+)
